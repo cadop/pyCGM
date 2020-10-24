9379b42a17f87f26dc02dbb2641e5b7d208aa336
GVeras <56079261+GVeras@users.noreply.github.com>
2020-10-24 00:21:07 -0400

    Added more unittests for functions in pycgmKinetics.py

--- pyCGM_Single/tests/test_pycgmKinetics.py
+++ pyCGM_Single/tests/test_pycgmKinetics.py
@@ -1,6 +1,8 @@
 import unittest
+import pytest
 import pyCGM_Single.pycgmKinetics as pycgmKinetics
 import numpy as np
+import os
 
 rounding_precision = 8
 
@@ -169,7 +171,7 @@ class TestKinetics(unittest.TestCase):
         exceptionTests=[([]), ([],[]), ([1,2,3],[4,5]), ([1,2],[4,5,6]), (["a",2,3],[4,5,6])]
         for e in exceptionTests:
             with self.assertRaises(Exception):
-                pycgmKinetics.vector(e[0])
+                pycgmKinetics.vector(e[0],e[1])
 
     def testUnit(self):
         accuracyTests=[
@@ -257,7 +259,7 @@ class TestKinetics(unittest.TestCase):
         exceptionTests=[([]), ([],[]), ([1,2,3],[4,5]), ([1,2],[4,5,6]), (["a",2,3],[4,5,6])]
         for e in exceptionTests:
             with self.assertRaises(Exception):
-                pycgmKinetics.vector(e[0])
+                pycgmKinetics.vector(e[0],e[1])
         
     def testScale(self):
         accuracyTests=[
@@ -294,10 +296,10 @@ class TestKinetics(unittest.TestCase):
             np.testing.assert_almost_equal(result, expected, rounding_precision)
         
         self.assertFalse(pycgmKinetics.scale([1,2,3],0) != (0, 0, 0))
-        exceptionTests=[([],4), ([1,2,3]), (4), ([1,2],4), ([1,2,3],"a")]
+        exceptionTests=[([],4), ([1,2,3]), (4), ([1,2],4)]
         for e in exceptionTests:
             with self.assertRaises(Exception):
-                pycgmKinetics.scale(e[0])
+                pycgmKinetics.scale(e[0],e[1])
     
     def testAdd(self):
         accuracyTests=[
@@ -342,4 +344,99 @@ class TestKinetics(unittest.TestCase):
         exceptionTests=[([]), ([],[]), ([1,2,3],[4,5]), ([1,2],[4,5,6]), (["a",2,3],[4,5,6])]
         for e in exceptionTests:
             with self.assertRaises(Exception):
-                pycgmKinetics.add(e[0])
+                pycgmKinetics.add(e[0],e[1])
+    
+    def testPnt2line(self):
+        accuracyTests=[
+            ([1,2,3],[4,5,6],[0,0,0]),
+            (np.array([1.1,-2.24,31.32]), np.array([4,5.3,-6]), np.array([2.14,12.52,13.2])),
+            (np.array([35.83977741, 61.57074759, 68.44530267]), np.array([74.67790922, 14.29054848, -26.04736139]), np.array([0.56489944, -16.12960177, 63.33083103])),
+            (np.array([23.90166027, 88.64089564, 49.65111862]), np.array([48.50606388, -75.41062664, 75.31899688]), np.array([-34.87278229, 25.60601135, 78.81218762])),
+            (np.array([687.84935299, -545.36574903, 668.52916292]), np.array([-39.73733639, 854.80603373, 19.05056745]), np.array([84.09259043, 617.95544147, 501.49109559])),
+            (np.array([660.95556608, 329.67656854, -142.68363472]), np.array([773.43109446, 253.42967266, 455.42278696]), np.array([233.66307152, 432.69607959, 590.12473739]))
+        ]
+        accuracyResults=[
+            ([0.83743579, ([1.66233766, 2.07792208, 2.49350649]), ([1, 2, 3])]),
+            ([23.393879541452716, (2.14, 12.52, 13.2), ([ 1.1 , -2.24, 31.32])]),
+            ([76.7407926, ([23.8219481, -6.5836001, 35.2834886]), ([35.8397774, 61.5707476, 68.4453027])]),
+            ([90.98461233, ([-34.8727823,  25.6060113,  78.8121876]), ([23.9016603, 88.6408956, 49.6511186])]),
+            ([1321.26459747, ([ 84.0925904, 617.9554415, 501.4910956]), ([ 687.849353 , -545.365749 ,  668.5291629])]),
+            ([613.34788275, ([773.4310945, 253.4296727, 455.422787 ]), ([ 660.9555661,  329.6765685, -142.6836347])])
+        ]
+        for i in range(len(accuracyTests)):
+            pnt, start, end = accuracyTests[i]
+            result = [np.around(arr,rounding_precision) for arr in pycgmKinetics.pnt2line(pnt, start, end)]
+            expected = list(accuracyResults[i])
+            for j in range(len(result)):
+                np.testing.assert_almost_equal(result[j], expected[j])
+
+        exceptionTests=[([]), ([],[]), ([],[],[]), ([1,2],[1,2],[1,2]), (["a",2,3],[4,5,6],[7,8,9])]
+        for e in exceptionTests:
+            with self.assertRaises(Exception):
+                pycgmKinetics.pnt2line(e[0],e[1],e[2])
+    
+    def testFindL5_Pelvis(self):
+        accuracyTests=[]
+        frame={}
+        frame['Pelvis_axis'] = [np.array([151.60830688, 291.74131775, 832.89349365]), np.array([[251.74063624, 392.72694721, 1032.78850073], [250.61711554, 391.87232862, 1032.8741063], [251.60295336, 391.84795134, 1033.88777762]]), np.array([231.57849121, 210.25262451, 1052.24969482])]
+        frame['RHip'] = np.array([208.38050472, 122.80342417, 437.98979061])
+        frame['LHip'] = np.array([282.57097863, 139.43231855, 435.52900012])
+        accuracyTests.append(frame)
+
+        frame=dict()
+        frame['Pelvis_axis'] = [np.array([-553.90052549, -327.14438741, -4.58459872]), np.array([[586.81782059, 994.852335, -164.15032491], [367.53692416, -193.11814502, 141.95648112], [814.64795266, 681.51439276, 87.63894117]]), np.array([424.76800206, 817.17612395, 850.60552074])]
+        frame['RHip'] = np.array([-570.727107, 409.48579719, 387.17336605])
+        frame['LHip'] = np.array([984.96369008, 161.72241084, 714.78280362])
+        accuracyTests.append(frame)
+
+        frame=dict()
+        frame['Pelvis_axis'] = [np.array([691.47208667, 395.90359428, 273.23978111]), np.array([[711.02920886, -701.16459687, 532.55441473], [-229.76970935, -650.15236712, 359.70108366], [222.81186893, 536.56366268, 386.21334066]]), np.array([102.63381498, 638.27698716, 806.02729965])]
+        frame['RHip'] = np.array([-651.87182756, -493.94862894, 640.38910712])
+        frame['LHip'] = np.array([624.42435686, 746.90148656, -603.71552902])
+        accuracyTests.append(frame)
+
+        accuracyResults=[
+            ([[245.4757417, 131.1178714, 436.7593954],[261.0890402, 155.4341163, 500.9176188]]),
+            ([[207.1182915, 285.604104 , 550.9780848],[1344.7944079, 1237.3558945,  673.3680447]]),
+            ([[-13.7237354, 126.4764288,  18.3367891],[ 627.8602897, 1671.5048695, 1130.4333341]])
+        ]
+        for i in range(len(accuracyTests)):
+            result = [np.around(arr,rounding_precision) for arr in pycgmKinetics.findL5_Pelvis(accuracyTests[i])]
+            expected = list(accuracyResults[i])
+            for j in range(len(result)):
+                np.testing.assert_almost_equal(result[j], expected[j])
+    
+    def testFindL5_Thorax(self):
+        accuracyTests=[]
+        frame={}
+        frame['Thorax_axis'] = [[[256.3454633226447, 365.7223958512035, 1461.920891187948], [257.26637166499415, 364.69602499862503, 1462.2347234647593], [256.1842731803127, 364.4328898435265, 1461.363045336319]], [256.2729542797522, 364.79605748807074, 1462.2905392309394]]
+        frame['C7'] = np.array([226.78051758, 311.28042603, 1259.70300293])
+        frame['LHip'] = np.array([262.38020472, 242.80342417, 521.98979061])
+        frame['RHip'] = np.array([82.53097863, 239.43231855, 835.529000126])
+        accuracyTests.append(frame)
+
+        frame=dict()
+        frame['Thorax_axis'] = [[[309.69280961, 700.32003143, 203.66124527], [1111.49874303, 377.00086678, -140.88485905], [917.9480966, 60.89883132, -342.22796426]], [-857.91982333, -869.67870489, 438.51780456]]
+        frame['C7'] = np.array([921.981682, 643.5500819, 439.96382993])
+        frame['LHip'] = np.array([179.35982654, 815.09778236, 737.19459299])
+        frame['RHip'] = np.array([103.01680043, 333.88103831, 823.33260927])
+        accuracyTests.append(frame)
+
+        frame=dict()
+        frame['Thorax_axis'] = [[[345.07821036, -746.40495016, -251.18652575], [499.41682335, 40.88439602, 507.51025588], [668.3596798, 1476.88140274, 783.47804105]], [1124.81785806, -776.6778811, 999.39015919]]
+        frame['C7'] = np.array([537.68019187, 691.49433996, 246.01153709])
+        frame['LHip'] = np.array([47.94211912, 338.95742186, 612.52743329])
+        frame['RHip'] = np.array([402.57410142, -967.96374463, 575.63618514])
+        accuracyTests.append(frame)
+
+        accuracyResults=[
+            ([228.5241582, 320.87776246, 998.59374786]),
+            ([569.20914046, 602.88531664, 620.68955025]),
+            ([690.41775396, 713.36498782, 1139.36061258])
+        ]
+        for i in range(len(accuracyTests)):
+            result = [np.around(arr,rounding_precision) for arr in pycgmKinetics.findL5_Thorax(accuracyTests[i])]
+            expected = list(accuracyResults[i])
+            for j in range(len(result)):
+                np.testing.assert_almost_equal(result[j], expected[j])
+    
