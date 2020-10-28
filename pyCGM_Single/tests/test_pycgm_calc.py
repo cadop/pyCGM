19d105388b6cdf5ad312f9693b111794133fb423
moppr <mantony42@gmail.com>
2020-10-28 01:55:00 -0400

    Create tests/test_pycgm_calc.py
    Signed-off-by: moppr <mantony42@gmail.com>

---
+++ pyCGM_Single/tests/test_pycgm_calc.py
@@ -0,0 +1,28 @@
+import unittest
+import pyCGM_Single.pycgmCalc as pycgmCalc
+import numpy as np
+from parameterized import parameterized
+
+rounding_precision = 8
+
+
+class TestPycgmCalc(unittest.TestCase):
+
+    # Attributes go here
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_calcKinetics(self):
+        # Single frame tests
+
+        pass
+
+    def test_calcAngles(self):
+        pass
+
+    def test_Calc(self):
+        pass
+
+    def test_calcFrames(self):
+        pass
