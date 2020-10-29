2cd5a32d396e01583e2ac062cae7776136a4f64d
moppr <mantony42@gmail.com>
2020-10-29 13:37:20 -0400

    Revert "Test experimental fix for setup.py workflow"
    This reverts commit 0c60236a

--- setup.py
+++ setup.py
@@ -3,12 +3,8 @@ sys.path.append('./pyCGM_Single') # TODO update to pycgm when fixed
 from _about import __version__
 import setuptools
 
-if sys.version_info.major == 2:
-    with open("README.md", "r") as fh:
-        long_description = fh.read()
-else:
-    with open("README.md", "r", encoding="utf8") as fh:
-        long_description = fh.read()
+with open("README.md", "r",encoding="utf8") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="pycgm", 
