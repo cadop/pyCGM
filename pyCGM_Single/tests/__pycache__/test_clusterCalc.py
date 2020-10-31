b2ea00b21922206bc0f171382b677def3c9fb839
GVeras <56079261+GVeras@users.noreply.github.com>
2020-10-30 21:45:27 -0400

    Created test_clusterCalc.py

---
+++ pyCGM_Single/tests/__pycache__/test_clusterCalc.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+import pytest
+import pyCGM_Single.clusterCalc as clusterCalc
+import numpy as np
+import os
+
+rounding_precision = 8
+
+class Test_clusterCalc(TestCase):
+    
