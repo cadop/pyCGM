ccce6a9079c8d5f09e7b5f682a6e4c1fa0169e79
moppr <mantony42@gmail.com>
2020-12-02 08:59:28 -0500

    Static docstrings

--- refactor/pycgm.py
+++ refactor/pycgm.py
@@ -621,7 +621,22 @@ class StaticCGM:
 
     @staticmethod
     def rotation_matrix(x=0, y=0, z=0):
-        pass
+        """Rotation Matrix function
+
+        This function creates and returns a rotation matrix.
+
+        Parameters
+        ----------
+        x, y, z : float, optional
+            Angle, which will be converted to radians, in
+            each respective axis to describe the rotations.
+            The default is 0 for each unspecified angle.
+
+        Returns
+        -------
+        rxyz : list
+            The product of the matrix multiplication.
+        """
 
     @staticmethod
     def get_dist(p0, p1):
@@ -629,27 +644,104 @@ class StaticCGM:
 
     @staticmethod
     def norm2d(v):
-        pass
+        """2D Vector normalization function
+
+        This function calculates the normalization of a 3-dimensional vector.
+
+        Parameters
+        ----------
+        v : list
+            A 3D vector.
+
+        Returns
+        -------
+        float
+            The normalization of the vector as a float.
+        """
 
     @staticmethod
     def norm3d(v):
-        pass
+        """3D Vector normalization function
+
+        This function calculates the normalization of a 3-dimensional vector.
+
+        Parameters
+        ----------
+        v : list
+            A 3D vector.
+
+        Returns
+        -------
+        list
+            The normalization of the vector returned as a float in an array.
+        """
 
     @staticmethod
     def matrix_mult(a, b):
-        pass
+        """Matrix multiplication function
+
+        This function returns the product of a matrix multiplication given two matrices.
+
+        Let the dimension of the matrix A be: m by n,
+        let the dimension of the matrix B be: p by q,
+        multiplication will only possible if n = p,
+        creating a matrix of m by q size.
+
+        Parameters
+        ----------
+        a : list
+            First matrix, in a 2D array format.
+        b : list
+            Second matrix, in a 2D array format.
+
+        Returns
+        -------
+        c : list
+            The product of the matrix multiplication.
+        """
 
     @staticmethod
     def cross(a, b):
-        pass
+        """Cross Product function
+
+        Given vectors a and b, calculate the cross product.
+
+        Parameters
+        ----------
+        a : list
+            First 3D vector.
+        b : list
+            Second 3D vector.
+
+        Returns
+        -------
+        list
+            The cross product of vector a and vector b.
+        """
 
     @staticmethod
     def average(list):
         pass
 
     @staticmethod
-    def find_joint_center(a, b, c, delta):
-        pass
+    def find_joint_c(a, b, c, delta):
+        """Calculate the Joint Center function.
+
+        This function is based on physical markers, a,b,c and joint center which will be
+        calulcated in this function are all in the same plane.
+
+        Parameters
+        ----------
+        a,b,c : list
+            Three markers x,y,z position of a, b, c.
+        delta : float
+            The length from marker to joint center, retrieved from subject measurement file.
+
+        Returns
+        -------
+        mr : array
+            Returns the Joint C x, y, z positions in a 1x3 array.
+        """
 
     @staticmethod
     def get_static():
@@ -667,29 +759,221 @@ class StaticCGM:
     def static_calculation():
         pass
 
-    @staticmethod
-    def pelvis_axis_calc():
-        pass
+@staticmethod
+    def pelvis_axis_calc(rasi, lasi, rpsi=None, lpsi=None, sacr=None):
+        """The Pelvis Axis Calculation function
+
+        Calculates the pelvis joint center and axis and returns both.
+
+        Markers used: RASI, LASI, RPSI, LPSI
+        Other landmarks used: origin, sacrum
+
+        Pelvis X_axis: Computed with a Gram-Schmidt orthogonalization procedure(ref. Kadaba 1990) and then normalized.
+        Pelvis Y_axis: LASI-RASI x,y,z positions, then normalized.
+        Pelvis Z_axis: CGM.cross product of x_axis and y_axis.
+
+        Parameters
+        ----------
+        rasi, lasi : array
+            A 1x3 array of each respective marker containing the XYZ positions.
+        rpsi, lpsi, sacr : array, optional
+            A 1x3 array of each respective marker containing the XYZ positions.
+
+        Returns
+        -------
+        array
+            Returns a 4x1x3 ndarray that contains the pelvis origin and the
+            pelvis x, y, and z axis components.
+
+        References
+        ----------
+        .. [12] Kadaba MP, Ramakrishnan HK, Wootten ME.
+           Measurement of lower extremity kinematics during level walking.
+           Journal of orthopaedic research: official publication of the Orthopaedic Research Society.
+           1990;8(3):383–92.
+        """
 
     @staticmethod
-    def hip_axis_calc():
-        pass
+    def hip_axis_calc(pel, measurements):
+        """The Hip Axis Calculation function
+
+        Calculates the hip joint center and returns the hip joint center.
+
+        Other landmarks used: origin, sacrum
+        Subject Measurement values used: MeanLegLength, R_AsisToTrocanterMeasure, 
+        InterAsisDistance, L_AsisToTrocanterMeasure
+
+        Hip Joint Center: Computed using Hip Joint Center Calculation (ref. Davis_1991)
+
+        Parameters
+        ----------
+        pel : array
+            A array containing a 1x3 array representing the pelvis origin, followed by
+            a 3x1x3 array representing the x, y, z components of the pelvis axis.
+        measurements : dict
+            A dictionary containing the subject measurements given from the file input.
+
+        Returns
+        -------
+        array
+            Returns an 8x1x3 ndarray that contains the right hip origin, right hip x, y, and z
+            axis components, left hip origin, and left hip x, y, and z axis components.
+
+        References
+        ----------
+        .. [20]  Davis RB, Ounpuu S, Tyburski D, Gage JR.
+           A gait analysis data collection and reduction technique. Human Movement Science.
+           1991;10(5):575–587.
+        """
 
     @staticmethod
-    def knee_axis_calc():
-        pass
+    def knee_axis_calc(rthi, lthi, rkne, lkne, hip_jc, delta, measurements):
+        """The Knee Axis Calculation function
+
+        Calculates the knee joint axis and returns the knee origin and axis
+
+        Markers used: RTHI, LTHI, RKNE, LKNE, hip_JC
+        Subject Measurement values used: RightKneeWidth, LeftKneeWidth
+
+        Knee joint center: Computed using Knee Axis Calculation(ref. Clinical Gait Analysis hand book, Baker2013)
+
+        Parameters
+        ----------
+        rthi, lthi, rkne, lkne : array
+            A 1x3 array of each respective marker containing the XYZ positions.
+        hip_jc : array
+            An array of hip_JC containing the x,y,z axes marker positions of the hip joint center.
+        delta : float
+            The length from marker to joint center, retrieved from subject measurement file.
+        measurements : dict
+            A dictionary containing the subject measurements given from the file input.
+
+        Returns
+        -------
+        array
+            Returns an 8x1x3 ndarray that contains the right knee origin, right knee x, y, and z
+            axis components, left knee origin, and left knee x, y, and z axis components.
+
+        Modifies
+        --------
+        delta is changed suitably to knee.
+
+        References
+        ----------
+        .. [43]  Baker R.
+           Measuring walking: a handbook of clinical gait analysis.
+           Hart Hilary M, editor. Mac Keith Press; 2013.
+        """
 
     @staticmethod
-    def ankle_axis_calc():
-        pass
+    def ankle_axis_calc(rtib, ltib, rank, lank, knee_jc, delta, measurements):
+        """The Ankle Axis Calculation function
+
+        Calculates the ankle joint axis and returns the ankle origin and axis
+
+        Markers used: RTIB, LTIB, RANK, LANK, knee_JC
+        Subject Measurement values used: RightKneeWidth, LeftKneeWidth
+
+        Ankle Axis: Computed using Ankle Axis Calculation(ref. Clinical Gait Analysis hand book, Baker2013).
+
+        Parameters
+        ----------
+        rtib, ltib, rank, lank : array
+            A 1x3 array of each respective marker containing the XYZ positions.
+        knee_jc : array
+            A numpy array representing the knee joint center origin and axis.
+        delta : float
+            The length from marker to joint center, retrieved from subject measurement file
+        measurements : dict, optional
+            A dictionary containing the subject measurements given from the file input.
+
+        Returns
+        -------
+        array
+            Returns an 8x1x3 ndarray that contains the right ankle origin, right ankle x, y, and z
+            axis components, left ankle origin, and left ankle x, y, and z axis components.
+
+        References
+        ----------
+        .. [43]  Baker R.
+           Measuring walking: a handbook of clinical gait analysis.
+           Hart Hilary M, editor. Mac Keith Press; 2013.
+        """
 
     @staticmethod
-    def foot_axis_calc():
-        pass
+    def foot_axis_calc(rtoe, ltoe, measurements, ankle_jc, knee_jc, delta):
+        """The Ankle Axis Calculation function
+
+        Calculate the foot joint axis by rotating incorrect foot joint axes about offset angle.
+        Returns the foot axis origin and axis.
+
+        In case of foot joint center, we've already make 2 kinds of axis for static offset angle.
+        and then, Call this static offset angle as an input of this function for dynamic trial.
+
+        Special Cases:
+
+        (anatomical uncorrect foot axis)
+        If foot flat is true, then make the reference markers instead of HEE marker
+        which height is as same as TOE marker's height.
+        otherwise, foot flat is false, use the HEE marker for making Z axis.
+
+        Markers used: RTOE, LTOE
+        Other landmarks used: ANKLE_FLEXION_AXIS
+        Subject Measurement values used: RightStaticRotOff, RightStaticPlantFlex, LeftStaticRotOff, LeftStaticPlantFlex
+
+        Parameters
+        ----------
+        rtoe, ltoe : array
+            A 1x3 array of each respective marker containing the XYZ positions.
+        measurements : dict
+            A dictionary containing the subject measurements given from the file input.
+        ankle_jc : array
+            An array containing the x,y,z axes marker positions of the ankle joint center.
+        knee_jc : array
+            An array containing the x,y,z axes marker positions of the knee joint center.
+        delta
+            The length from marker to joint center, retrieved from subject measurement file.
+
+        Returns
+        -------
+        array
+            Returns an 8x1x3 ndarray that contains the right foot origin, right foot x, y, and z
+            axis components, left foot origin, and left foot x, y, and z axis components.
+
+        Modifies
+        --------
+        Axis changes following to the static info.
+
+        you can set the static_info by the button. and this will calculate the offset angles
+        the first setting, the foot axis show foot uncorrected anatomical reference axis(Z_axis point to the AJC from TOE)
+
+        if press the static_info button so if static_info is not None,
+        and then the static offsets angles are applied to the reference axis.
+        the reference axis is Z axis point to HEE from TOE
+        """
 
     @staticmethod
-    def head_axis_calc():
-        pass
+    def head_axis_calc(lfhd, rfhd, lbhd, rbhd, measurements):
+        """The Head Axis Calculation function
+
+        Calculates the head joint center and returns the head joint center and axis.
+
+        Markers used: LFHD, RFHD, LBHD, RBHD
+        Subject Measurement values used: HeadOffset
+
+        Parameters
+        ----------
+        lfhd, rfhd, lbhd, rbhd : array
+            A 1x3 array of each respective marker containing the XYZ positions.
+        measurements : dict
+            A dictionary containing the subject measurements given from the file input.
+
+        Returns
+        -------
+        array
+            Returns a 4x1x3 ndarray that contains the head origin and the
+            head x, y, and z axis components.
+        """
 
     @staticmethod
     def ankle_angle_calc():
