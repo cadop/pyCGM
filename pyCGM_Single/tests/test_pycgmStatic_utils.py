90e1355dbedfcc26708548556d493f5277d5819e
thepeytongreen <71730256+thepeytongreen@users.noreply.github.com>
2020-12-04 01:00:56 -0600

    Revert "Create test_pycgmStatic_utils.py"
    This reverts commit 1cd64c43424d44270a331055700983fc655b8ee4.

--- pyCGM_Single/tests/test_pycgmStatic_utils.py
+++ pyCGM_Single/tests/test_pycgmStatic_utils.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pytest
 import numpy as np
 import os
@@ -928,12 +927,3 @@ class TestPycgmStaticGetStatic():
         np.testing.assert_almost_equal(result['LeftStaticRotOff'], 0.0094029102924030206, rounding_precision)
         np.testing.assert_almost_equal(result['LeftStaticPlantFlex'], 0.20251085737834015, rounding_precision)
         np.testing.assert_almost_equal(result['HeadOffset'], 0.25719904693106527, rounding_precision)
-=======
-import unittest
-import pyCGM_Single.pycgmStatic as pyCGM
-import numpy as np
-
-rounding_precision = 6
-
-class TestPycgmStaticUtils(unittest.TestCase):
->>>>>>> 9d9a211... Create test_pycgmStatic_utils.py
