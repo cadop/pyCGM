e67e08dbf33ca43ca538ad9a12ec9840ce905f14
moppr <mantony42@gmail.com>
2020-12-04 20:02:50 -0500

    Test runtime

--- refactor/user_sample.py
+++ refactor/user_sample.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import os
+import timeit
 
 if __name__ == "__main__":
     from refactor.pycgm import CGM
@@ -13,7 +14,15 @@ if __name__ == "__main__":
     measurements = sample_dir + "Sample_SM.vsk"
     subject1 = CGM(static_trial, dynamic_trial, measurements)
     subject1.run()
-    print(subject1.axis_results[0])
+    setup = '''from refactor.pycgm import CGM
+sample_dir = "../SampleData/ROM/"
+static_trial = sample_dir + "Sample_Static.c3d"
+dynamic_trial = sample_dir + "Sample_Dynamic.c3d"
+measurements = sample_dir + "Sample_SM.vsk"
+subject1 = CGM(static_trial, dynamic_trial, measurements)'''
+    code = '''subject1.run()'''
+    print(timeit.timeit(code, setup, number=1), "sec")
+    # print(subject1.axis_results[0])
 
     # measurements = {'MeanLegLength': 940.0, 'R_AsisToTrocanterMeasure': 72.512,
     #                 'L_AsisToTrocanterMeasure': 72.512, 'InterAsisDistance': 215.908996582031}
