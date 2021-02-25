2e298adaeeb65d9bca54bc0c511a6301b88630a8
mgonz1228 <mgonz1228@gmail.com>
2021-02-24 19:56:54 -0500

    undo rounding of writeResult doctest

--- pyCGM_Single/pycgmIO.py
+++ pyCGM_Single/pycgmIO.py
@@ -880,10 +880,10 @@ def writeResult(data, filename, **kargs):
         for the first joint, the pelvis, and axis values for the pelvis origin, PELO.
 
         >>> frame = zeros(273)
-        >>> angles = array([-0.31, -6.12, 7.57])
+        >>> angles = array([-0.308494914509454, -6.12129279337001, 7.57143110215171])
         >>> for i in range(len(angles)):
         ...     frame[i] = angles[i]
-        >>> axis = array(['-934.31', '-4.44', '852.84'])
+        >>> axis = array(['-934.314880371094', '-4.44443511962891', '852.837829589844'])
         >>> for i in range(len(axis)):
         ...     frame[i+57] = axis[i]
         >>> data = [frame]
@@ -897,7 +897,7 @@ def writeResult(data, filename, **kargs):
         >>> result = lines[7].strip().split(',')
         >>> result #doctest: +NORMALIZE_WHITESPACE
         ['0.000000000000000',
-         '-0.31', '-6.12', '7.57',...]
+         '-0.308494914509454', '-6.121292793370010', '7.571431102151710',...]
 
         Writing axis only.
 
@@ -908,7 +908,7 @@ def writeResult(data, filename, **kargs):
         >>> result = lines[7].strip().split(',')
         >>> result #doctest: +NORMALIZE_WHITESPACE
         ['0.000000000000000',
-         '-934.31', '-4.44', '852.84',...]
+         '-934.314880371093977', '-4.444435119628910', '852.837829589843977',...]
         >>> rmtree(tmpdirName)
         """
         labelsAngs =['Pelvis','R Hip','L Hip','R Knee','L Knee','R Ankle',
