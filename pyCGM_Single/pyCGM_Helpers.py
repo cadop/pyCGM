1551c5f80098f6ce5262fb1e564b7ead83283e90
khgaw <kgaw2017@gmail.com>
2021-03-01 18:24:42 -0500

    Fix formatting

--- pyCGM_Single/pyCGM_Helpers.py
+++ pyCGM_Single/pyCGM_Helpers.py
@@ -19,16 +19,16 @@ def getfilenames(x=1):
     -------
     dynamic_trial, static_trial, vsk_file, outputfile, CoM_output : tuple
         Returns the respective filenames in the relative path.
-    
+
     Example
     -------
     >>> import os 
     >>> from .pyCGM_Helpers import getfilenames
     >>> getfilenames() #doctest: +NORMALIZE_WHITESPACE
-    ('SampleData/59993_Frame/59993_Frame_Dynamic.c3d', 
-    'SampleData/59993_Frame/59993_Frame_Static.c3d', 
-    'SampleData/59993_Frame/59993_Frame_SM.vsk', 
-    'SampleData/59993_Frame/pycgm_results.csv', 
+    ('SampleData/59993_Frame/59993_Frame_Dynamic.c3d',
+    'SampleData/59993_Frame/59993_Frame_Static.c3d',
+    'SampleData/59993_Frame/59993_Frame_SM.vsk',
+    'SampleData/59993_Frame/pycgm_results.csv',
     'SampleData/59993_Frame/CoM')
     >>> getfilenames(2) #doctest: +NORMALIZE_WHITESPACE
     ('SampleData/ROM/Sample_Dynamic.c3d',
@@ -46,28 +46,28 @@ def getfilenames(x=1):
     scriptdir = os.path.dirname(os.path.abspath(__file__))
     os.chdir( scriptdir )
     os.chdir( ".." ) #move current path one up to the directory of pycgm_embed
-    
+
     if x == 1:
         dir = 'SampleData/59993_Frame/'
-        dynamic_trial = dir+'59993_Frame_Dynamic.c3d' 
-        static_trial =  dir+'59993_Frame_Static.c3d' 
-        vsk_file =      dir+'59993_Frame_SM.vsk'     
+        dynamic_trial = dir+'59993_Frame_Dynamic.c3d'
+        static_trial =  dir+'59993_Frame_Static.c3d'
+        vsk_file =      dir+'59993_Frame_SM.vsk' 
         outputfile =    dir+'pycgm_results.csv'
         CoM_output =    dir+'CoM'
-        
+
     if x == 2:
         dir = 'SampleData/ROM/'
-        dynamic_trial = dir+'Sample_Dynamic.c3d' 
-        static_trial =  dir+'Sample_Static.c3d' 
-        vsk_file =      dir+'Sample_SM.vsk'     
+        dynamic_trial = dir+'Sample_Dynamic.c3d'
+        static_trial =  dir+'Sample_Static.c3d'
+        vsk_file =      dir+'Sample_SM.vsk' 
         outputfile =    dir+'pycgm_results.csv'
         CoM_output =    dir+'CoM'
-        
+
     if x == 3:
         dir = 'SampleData/Sample_2/'
-        dynamic_trial = dir+'RoboWalk.c3d' 
-        static_trial =  dir+'RoboStatic.c3d' 
-        vsk_file =      dir+'RoboSM.vsk'     
+        dynamic_trial = dir+'RoboWalk.c3d'
+        static_trial =  dir+'RoboStatic.c3d'
+        vsk_file =      dir+'RoboSM.vsk' 
         outputfile =    dir+'pycgm_results.csv'
         CoM_output =    dir+'CoM'
     
