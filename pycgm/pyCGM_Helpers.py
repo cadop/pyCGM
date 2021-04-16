60a147a80e2979053b22636b1a1faa298c58af36
khgaw <kgaw2017@gmail.com>
2021-04-15 21:29:19 -0400

    Fix variable names

--- pycgm/pyCGM_Helpers.py
+++ pycgm/pyCGM_Helpers.py
@@ -45,32 +45,32 @@ def getfilenames(x=1):
     'SampleData/Sample_2/pycgm_results.csv',
     'SampleData/Sample_2/CoM')
     """
-    scriptdir = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(scriptdir)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(script_dir)
     os.chdir("..")  # move current path one up to the directory of pycgm_embed
 
     if x == 1:
         dir = 'SampleData/59993_Frame/'
-        dynamic_trial = dir+'59993_Frame_Dynamic.c3d'
-        static_trial = dir+'59993_Frame_Static.c3d'
-        vsk_file = dir+'59993_Frame_SM.vsk'
-        outputfile = dir+'pycgm_results.csv'
-        CoM_output = dir+'CoM'
+        dynamic_trial = dir + '59993_Frame_Dynamic.c3d'
+        static_trial = dir + '59993_Frame_Static.c3d'
+        vsk_file = dir + '59993_Frame_SM.vsk'
+        output_file = dir + 'pycgm_results.csv'
+        CoM_output = dir + 'CoM'
 
     if x == 2:
         dir = 'SampleData/ROM/'
-        dynamic_trial = dir+'Sample_Dynamic.c3d'
-        static_trial = dir+'Sample_Static.c3d'
-        vsk_file = dir+'Sample_SM.vsk'
-        outputfile = dir+'pycgm_results.csv'
-        CoM_output = dir+'CoM'
+        dynamic_trial = dir + 'Sample_Dynamic.c3d'
+        static_trial = dir + 'Sample_Static.c3d'
+        vsk_file = dir + 'Sample_SM.vsk'
+        output_file = dir + 'pycgm_results.csv'
+        CoM_output = dir + 'CoM'
 
     if x == 3:
         dir = 'SampleData/Sample_2/'
-        dynamic_trial = dir+'RoboWalk.c3d'
-        static_trial = dir+'RoboStatic.c3d'
-        vsk_file = dir+'RoboSM.vsk'
-        outputfile = dir+'pycgm_results.csv'
-        CoM_output = dir+'CoM'
+        dynamic_trial = dir + 'RoboWalk.c3d'
+        static_trial = dir + 'RoboStatic.c3d'
+        vsk_file = dir + 'RoboSM.vsk'
+        output_file = dir + 'pycgm_results.csv'
+        CoM_output = dir + 'CoM'
 
-    return dynamic_trial, static_trial, vsk_file, outputfile, CoM_output
+    return dynamic_trial, static_trial, vsk_file, output_file, CoM_output
