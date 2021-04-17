0eb150ce65866700755c7b1eb6c7ac5702ace8d5
AmeerYHassan <ayh7@njit.edu>
2021-04-17 16:49:47 -0400

    refactoring of find_joint_center function

--- pycgm/axis.py
+++ pycgm/axis.py
@@ -28,155 +28,74 @@
 This file is used in joint angle and joint center calculations.
 """
 
-import sys
-import os
-from math import sin, cos, pi, sqrt
 import math
 import numpy as np
 
-def cross(vec_a, vec_b):
-    """Cross Product.
-    Given vectors vec_a and vec_b, calculate the cross product.
+def find_joint_center(mark_a, mark_b, mark_c, delta):
+    """Calculate the Joint Center.
+    This function is based on physical markers, a,b,c and joint center which
+    will be calulcated in this function are all in the same plane.
     Parameters
     ----------
-    vec_a : list
-        First 3D vector.
-    vec_b : list
-        Second 3D vector.
+    mark_a, mark_b, mark_c : list
+        Three markers x,y,z position of a, b, c.
+    delta : float
+        The length from marker to joint center, retrieved from subject measurement file.
     Returns
     -------
-    vec_c : list
-        The cross product of vec_a and vec_b.
+    joint_center : array
+        Returns the Joint C x, y, z positions in a 1x3 array.
     Examples
     --------
     >>> import numpy as np
-    >>> from .axis import cross
-    >>> vec_a = [6.25, 7.91, 18.63]
-    >>> vec_b = [3.49, 4.42, 19.23]
-    >>> np.around(cross(vec_a, vec_b), 2)
-    array([ 6.976e+01, -5.517e+01,  2.000e-02])
+    >>> from .axis import find_joint_center
+    >>> mark_a = [468.14, 325.09, 673.12]
+    >>> mark_b = [355.90, 365.38, 940.69]
+    >>> mark_c = [452.35, 329.06, 524.77]
+    >>> delta = 59.5
+    >>> find_joint_center(mark_a, mark_b, mark_c, delta).round(2)
+    array([396.25, 347.92, 518.63])
     """
-    vec_c = [vec_a[1]*vec_b[2] - vec_a[2]*vec_b[1],
-        vec_a[2]*vec_b[0] - vec_a[0]*vec_b[2],
-        vec_a[0]*vec_b[1] - vec_a[1]*vec_b[0]]
+    # make the two vector using 3 markers, which is on the same plane.
+    vec_1 = (mark_a[0]-mark_c[0], mark_a[1]-mark_c[1], mark_a[2]-mark_c[2])
+    vec_2 = (mark_b[0]-mark_c[0], mark_b[1]-mark_c[1], mark_b[2]-mark_c[2])
 
-    return vec_c
+    # vec_3 is cross vector of vec_1, vec_2
+    # and then it normalized.
+    # vec_3 = cross(vec_1, vec_2)
+    vec_3 = np.cross(vec_1, vec_2)
+    vec_3_div = np.linalg.norm(vec_3)
+    vec_3 = [vec_3[0]/vec_3_div, vec_3[1]/vec_3_div, vec_3[2]/vec_3_div]
 
-def norm_div(vec):
-    """Normalized divison.
+    mid = [(mark_b[0]+mark_c[0])/2.0, (mark_b[1]+mark_c[1])/2.0, (mark_b[2]+mark_c[2])/2.0]
+    length = np.subtract(mark_b, mid)
+    length = np.linalg.norm(length)
 
-    This function calculates the normalization division of a 3-dimensional vector.
+    theta = math.acos(delta/np.linalg.norm(vec_2))
 
-    Parameters
-    ----------
-    vec : list
-        A 3D vector.
+    cs_th = math.cos(theta*2)
+    sn_th = math.sin(theta*2)
 
-    Returns
-    -------
-    array
-        The divison normalization of the vector returned as a float in an array.
+    u_x = vec_3[0]
+    u_y = vec_3[1]
+    u_z = vec_3[2]
 
-    Examples
-    --------
-    >>> import numpy as np
-    >>> from .axis import norm_div
-    >>> vec = [1.44, 1.94, 2.49]
-    >>> np.around(norm_div(vec), 2)
-    array([0.12, 0.16, 0.21])
-    """
-    try:
-        vec_b = sqrt((vec[0]*vec[0]+vec[1]*vec[1]+vec[2]*vec[2]))
-        vec = [vec[0]/vec_b, vec[1]/vec_b, vec[2]/vec_b]
-    except:
-        vec_b = np.nan
+    # this rotation matrix is called Rodriques' rotation formula.
+    # In order to make a plane, at least 3 number of markers is required which
+    # means three physical markers on the segment can make a plane.
+    # then the orthogonal vector of the plane will be rotating axis.
+    # joint center is determined by rotating the one vector of plane around rotating axis.
 
-    return [vec[0]/vec_b, vec[1]/vec_b, vec[2]/vec_b]
+    rot = np.matrix([
+        [cs_th+u_x**2.0*(1.0-cs_th),u_x*u_y*(1.0-cs_th)-u_z*sn_th,u_x*u_z*(1.0-cs_th)+u_y*sn_th],
+        [u_y*u_x*(1.0-cs_th)+u_z*sn_th,cs_th+u_y**2.0*(1.0-cs_th),u_y*u_z*(1.0-cs_th)-u_x*sn_th],
+        [u_z*u_x*(1.0-cs_th)-u_y*sn_th,u_z*u_y*(1.0-cs_th)+u_x*sn_th,cs_th+u_z**2.0*(1.0-cs_th)]
+    ])
 
-def matrixmult (matr_a, matr_b):
-    """Matrix multiplication.
+    r_vec = rot * (np.matrix(vec_2).transpose())
+    r_vec = r_vec * length/np.linalg.norm(r_vec)
 
-    This function returns the product of a matrix multiplication given two matrices.
+    r_vec = [r_vec[0,0], r_vec[1,0], r_vec[2,0]]
+    joint_center = np.array([r_vec[0]+mid[0], r_vec[1]+mid[1], r_vec[2]+mid[2]])
 
-    Let the dimension of the matrix A be: m by n,
-    let the dimension of the matrix B be: p by q,
-    multiplication will only possible if n = p,
-    creating a matrix of m by q size.
-
-    Parameters
-    ----------
-    matr_a : list
-        First matrix, in a 2D array format.
-    matr_b : list
-        Second matrix, in a 2D array format.
-
-    Returns
-    -------
-    matr_ab : list
-        The product of the matrix multiplication.
-
-    Examples
-    --------
-    >>> from .axis import matrixmult
-    >>> matr_a = [[11,12,13],[14,15,16]]
-    >>> matr_b = [[1,2],[3,4],[5,6]]
-    >>> matrixmult(matr_a, matr_b)
-    [[112, 148], [139, 184]]
-    """
-
-    matr_ab = [[0 for row in range(len(matr_a))] for col in range(len(matr_b[0]))]
-    for i in range(len(matr_a)):
-        for j in range(len(matr_b[0])):
-            for k in range(len(matr_b)):
-                matr_ab[i][j] += matr_a[i][k]*matr_b[k][j]
-    return matr_ab
-
-
-def rotmat(x=0, y=0, z=0):
-    """Rotation Matrix.
-
-    This function creates and returns a rotation matrix.
-
-    Parameters
-    ----------
-    x, y, z : float, optional
-        Angle, which will be converted to radians, in
-        each respective axis to describe the rotations.
-        The default is 0 for each unspecified angle.
-
-    Returns
-    -------
-    r_xyz : list
-        The product of the matrix multiplication.
-
-    Examples
-    --------
-    >>> import numpy as np
-    >>> from .axis import rotmat
-    >>> x = 0.5
-    >>> y = 0.3
-    >>> z = 0.8
-    >>> np.around(rotmat(x, y, z), 2) #doctest: +NORMALIZE_WHITESPACE
-    array([[ 1.  , -0.01,  0.01],
-    [ 0.01,  1.  , -0.01],
-    [-0.01,  0.01,  1.  ]])
-    >>> x = 0.5
-    >>> np.around(rotmat(x), 2) #doctest: +NORMALIZE_WHITESPACE
-    array([[ 1.  ,  0.  ,  0.  ],
-    [ 0.  ,  1.  , -0.01],
-    [ 0.  ,  0.01,  1.  ]])
-    >>> x = 1
-    >>> y = 1
-    >>> np.around(rotmat(x,y), 2) #doctest: +NORMALIZE_WHITESPACE
-    array([[ 1.  ,  0.  ,  0.02],
-    [ 0.  ,  1.  , -0.02],
-    [-0.02,  0.02,  1.  ]])
-    """
-    x, y, z = math.radians(x), math.radians(y), math.radians(z)
-    r_x = [ [1,0,0],[0,math.cos(x),math.sin(x)*-1],[0,math.sin(x),math.cos(x)] ]
-    r_y = [ [math.cos(y),0,math.sin(y)],[0,1,0],[math.sin(y)*-1,0,math.cos(y)] ]
-    r_z = [ [math.cos(z),math.sin(z)*-1,0],[math.sin(z),math.cos(z),0],[0,0,1] ]
-    r_xy = matrixmult(r_x,r_y)
-    r_xyz = matrixmult(r_xy,r_z)
-
-    return r_xyz
+    return joint_center
