60ec4ea8df47ca9404c72b0019d938bf4417ec28
David Kim <d@vid.kim>
2021-04-17 23:05:09 -0400

    Refactor hip axis calculation

--- pycgm/axis.py
+++ pycgm/axis.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import math
 import numpy as np
 
 
@@ -102,3 +103,196 @@ def pelvis_axis(rasi, lasi, rpsi, lpsi, sacr=None):
     pelvis[:3, 3] = o
 
     return pelvis
+
+
+def hip_joint_center(pelvis, subject):
+    u"""Get the right and left hip joint center.
+
+    Takes in a 4x4 affine matrix of pelvis axis and subject measurements
+    dictionary. Calculates and returns the left and right hip joint centers.
+
+    Subject Measurement values used: MeanLegLength, R_AsisToTrocanterMeasure,
+    InterAsisDistance, L_AsisToTrocanterMeasure
+
+    Hip Joint Center: Computed using Hip Joint Center Calculation [1]_.
+
+    Parameters
+    ----------
+    pelvis : array
+        A 4x4 affine matrix with pelvis x, y, z axes and pelvis origin.
+    subject : dict
+        A dictionary containing subject measurements.
+
+    Returns
+    -------
+    hip_JC : array
+        Returns a 2x3 array that contains two 1x3 arrays
+        containing the x, y, z components of the left and right hip joint
+        centers.
+
+    References
+    ----------
+    .. [1] Davis, R. B., III, Õunpuu, S., Tyburski, D. and Gage, J. R. (1991).
+            A gait analysis data collection and reduction technique.
+            Human Movement Science 10 575–87.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from .axis import hip_joint_center
+    >>> vsk = {'MeanLegLength': 940.0, 'R_AsisToTrocanterMeasure': 72.51,
+    ...        'L_AsisToTrocanterMeasure': 72.51, 'InterAsisDistance': 215.90}
+    >>> pelvis_axis = np.array([
+    ...     [0.14, 0.98, -0.11, 251.60],
+    ...     [-0.99, 0.13, -0.02, 391.74],
+    ...     [0, 0.1, 0.99, 1032.89],
+    ...     [0, 0, 0, 1]
+    ... ])
+    >>> np.around(hip_joint_center(pelvis_axis,vsk), 2) #doctest: +NORMALIZE_WHITESPACE
+    array([[181.71, 340.33, 936.18],
+    [307.36, 323.83, 938.72]])
+    """
+
+    # Requires
+    # pelvis axis
+
+    pel_origin = pelvis[:3, 3]
+
+    # Model's eigen value
+    #
+    # LegLength
+    # MeanLegLength
+    # mm (marker radius)
+    # interAsisMeasure
+
+    # Set the variables needed to calculate the joint angle
+    # Half of marker size
+    mm = 7.0
+
+    mean_leg_length = subject['MeanLegLength']
+    right_asis_to_trochanter = subject['R_AsisToTrocanterMeasure']
+    left_asis_to_trochanter = subject['L_AsisToTrocanterMeasure']
+    interAsisMeasure = subject['InterAsisDistance']
+    C = (mean_leg_length * 0.115) - 15.3
+    theta = 0.500000178813934
+    beta = 0.314000427722931
+    aa = interAsisMeasure/2.0
+    S = -1
+
+    # Hip Joint Center Calculation (ref. Davis_1991)
+
+    # Left: Calculate the distance to translate along the pelvis axis
+    L_Xh = (-left_asis_to_trochanter - mm) * \
+        math.cos(beta) + C * math.cos(theta) * math.sin(beta)
+    L_Yh = S*(C*math.sin(theta) - aa)
+    L_Zh = (-left_asis_to_trochanter - mm) * \
+        math.sin(beta) - C * math.cos(theta) * math.cos(beta)
+
+    # Right:  Calculate the distance to translate along the pelvis axis
+    R_Xh = (-right_asis_to_trochanter - mm) * \
+        math.cos(beta) + C * math.cos(theta) * math.sin(beta)
+    R_Yh = (C*math.sin(theta) - aa)
+    R_Zh = (-right_asis_to_trochanter - mm) * \
+        math.sin(beta) - C * math.cos(theta) * math.cos(beta)
+
+    # get the unit pelvis axis
+    pelvis_xaxis = pelvis[0, :3]
+    pelvis_yaxis = pelvis[1, :3]
+    pelvis_zaxis = pelvis[2, :3]
+    pelvis_axis = pelvis[:3, :3]
+
+    # multiply the distance to the unit pelvis axis
+    left_hip_jc_x = pelvis_xaxis * L_Xh
+    left_hip_jc_y = pelvis_yaxis * L_Yh
+    left_hip_jc_z = pelvis_zaxis * L_Zh
+    # left_hip_jc = left_hip_jc_x + left_hip_jc_y + left_hip_jc_z
+
+    left_hip_jc = np.asarray([
+        left_hip_jc_x[0]+left_hip_jc_y[0]+left_hip_jc_z[0],
+        left_hip_jc_x[1]+left_hip_jc_y[1]+left_hip_jc_z[1],
+        left_hip_jc_x[2]+left_hip_jc_y[2]+left_hip_jc_z[2]
+    ])
+
+    left_hip_jc = pelvis_axis.T @ np.array([L_Xh, L_Yh, L_Zh])
+
+    R_hipJCx = pelvis_xaxis * R_Xh
+    R_hipJCy = pelvis_yaxis * R_Yh
+    R_hipJCz = pelvis_zaxis * R_Zh
+    right_hip_jc = R_hipJCx + R_hipJCy + R_hipJCz
+
+    right_hip_jc = pelvis_axis.T @ np.array([R_Xh, R_Yh, R_Zh])
+
+    left_hip_jc = left_hip_jc+pel_origin
+    right_hip_jc = right_hip_jc+pel_origin
+
+    return np.array([left_hip_jc, right_hip_jc])
+
+
+def hip_axis(r_hip_jc, l_hip_jc, pelvis_axis):
+    r"""Make the hip axis.
+
+    Takes in the x, y, z positions of left and right hip joint center and
+    pelvis axis to calculate the hip axis.
+
+    hip origin: Midpoint of left and right hip joint centers.
+
+    Hip axis: sets the pelvis orientation to the hip center axis (i.e.
+    midpoint of left and right hip joint centers)
+
+    Parameters
+    ----------
+    l_hip_jc, r_hip_jc : array
+        left and right hip joint center with x, y, z position in an array.
+    pelvis_axis : array
+        4x4 affine matrix with pelvis x, y, z axes and pelvis origin.
+
+    Returns
+    -------
+    hipaxis_center, axis : array
+        4x4 affine matrix with hip x, y, z axes and hip origin.
+
+    .. math::
+
+        \begin{bmatrix}
+            \hat{x}_x & \hat{x}_y & \hat{x}_z & o_x \\
+            \hat{y}_x & \hat{y}_y & \hat{y}_z & o_y \\
+            \hat{z}_x & \hat{z}_y & \hat{z}_z & o_z \\
+            0 & 0 & 0 & 1 \\
+        \end{bmatrix}
+
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from .axis import hip_axis
+    >>> r_hip_jc = [182.57, 339.43, 935.52]
+    >>> l_hip_jc = [308.38, 322.80, 937.98]
+    >>> pelvis_axis = np.array([
+    ...     [0.14, 0.98, -0.11, 251.60],
+    ...     [-0.99, 0.13, -0.02, 391.74],
+    ...     [0, 0.1, 0.99, 1032.89],
+    ...     [0, 0, 0, 1]
+    ... ])
+    >>> [np.around(arr, 2) for arr in hip_axis(l_hip_jc,r_hip_jc, pelvis_axis)] #doctest: +NORMALIZE_WHITESPACE
+    [array([ 1.4000e-01,  9.8000e-01, -1.1000e-01,  2.4548e+02]),
+    array([-9.9000e-01,  1.3000e-01, -2.0000e-02,  3.3112e+02]),
+    array([0.0000e+00, 1.0000e-01, 9.9000e-01, 9.3675e+02]),
+    array([0., 0., 0., 1.])]
+    """
+
+    # Get shared hip axis, it is inbetween the two hip joint centers
+    hipaxis_center = (np.asarray(r_hip_jc) + np.asarray(l_hip_jc)) / 2.0
+
+    # convert pelvis_axis to x,y,z axis to use more easy
+    pelvis_x_axis = pelvis_axis[0, :3]
+    pelvis_y_axis = pelvis_axis[1, :3]
+    pelvis_z_axis = pelvis_axis[2, :3]
+
+    axis = np.zeros((4, 4))
+    axis[3, 3] = 1.0
+    axis[0, :3] = pelvis_x_axis
+    axis[1, :3] = pelvis_y_axis
+    axis[2, :3] = pelvis_z_axis
+    axis[:3, 3] = hipaxis_center
+
+    return axis
--- pycgm/tests/test_pycgm_axis.py
+++ pycgm/tests/test_pycgm_axis.py
@@ -230,3 +230,233 @@ class TestLowerBodyAxis:
 
         result = axis.pelvis_axis(rasi, lasi, rpsi, lpsi, sacr)
         np.testing.assert_almost_equal(result, expected, rounding_precision)
+
+    @pytest.mark.parametrize(["pel_origin", "pel_x", "pel_y", "pel_z", "vsk", "expected"], [
+        # Test from running sample data
+        ([251.608306884766, 391.741317749023, 1032.893493652344], [251.740636241119, 392.726947206848, 1032.788500732036], [250.617115540376, 391.872328624646, 1032.874106304030], [251.602953357582, 391.847951338178, 1033.887777624562],
+         {'MeanLegLength': 940.0, 'R_AsisToTrocanterMeasure': 72.512,
+             'L_AsisToTrocanterMeasure': 72.512, 'InterAsisDistance': 215.908996582031},
+         [[182.57097863, 339.43231855, 935.52900126], [308.38050472, 322.80342417, 937.98979061]]),
+        # Basic test with zeros for all params
+        ([0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
+         {'MeanLegLength': 0.0, 'R_AsisToTrocanterMeasure': 0.0,
+             'L_AsisToTrocanterMeasure': 0.0, 'InterAsisDistance': 0.0},
+         [[0, 0, 0], [0, 0, 0]]),
+        # Testing when values are added to pel_origin
+        ([1, 0, -3], [0, 0, 0], [0, 0, 0], [0, 0, 0],
+         {'MeanLegLength': 0.0, 'R_AsisToTrocanterMeasure': 0.0,
+             'L_AsisToTrocanterMeasure': 0.0, 'InterAsisDistance': 0.0},
+         [[-6.1387721, 0, 18.4163163], [8.53165418, 0, -25.59496255]]),
+        # Testing when values are added to pel_x
+        ([0, 0, 0], [-5, -3, -6], [0, 0, 0], [0, 0, 0],
+         {'MeanLegLength': 0.0, 'R_AsisToTrocanterMeasure': 0.0,
+             'L_AsisToTrocanterMeasure': 0.0, 'InterAsisDistance': 0.0},
+         [[54.02442793, 32.41465676, 64.82931352], [54.02442793, 32.41465676, 64.82931352]]),
+        # Testing when values are added to pel_y
+        ([0, 0, 0], [0, 0, 0], [4, -1, 2], [0, 0, 0],
+         {'MeanLegLength': 0.0, 'R_AsisToTrocanterMeasure': 0.0,
+             'L_AsisToTrocanterMeasure': 0.0, 'InterAsisDistance': 0.0},
+         [[29.34085257, -7.33521314, 14.67042628], [-29.34085257,   7.33521314, -14.67042628]]),
+        # Testing when values are added to pel_z
+        ([0, 0, 0], [0, 0, 0], [0, 0, 0], [3, 8, 2],
+         {'MeanLegLength': 0.0, 'R_AsisToTrocanterMeasure': 0.0,
+             'L_AsisToTrocanterMeasure': 0.0, 'InterAsisDistance': 0.0},
+         [[31.82533363, 84.86755635, 21.21688909], [31.82533363, 84.86755635, 21.21688909]]),
+        # Test when values are added to pel_x, pel_y, and pel_z
+        ([0, 0, 0], [-5, -3, -6], [4, -1, 2], [3, 8, 2],
+         {'MeanLegLength': 0.0, 'R_AsisToTrocanterMeasure': 0.0,
+             'L_AsisToTrocanterMeasure': 0.0, 'InterAsisDistance': 0.0},
+         [[115.19061413, 109.94699997, 100.71662889], [56.508909, 124.61742625,  71.37577632]]),
+        # Test when values are added to pel_origin, pel_x, pel_y, and pel_z
+        ([1, 0, -3], [-5, -3, -6], [4, -1, 2], [3, 8, 2],
+         {'MeanLegLength': 0.0, 'R_AsisToTrocanterMeasure': 0.0,
+             'L_AsisToTrocanterMeasure': 0.0, 'InterAsisDistance': 0.0},
+         [[109.05184203, 109.94699997, 119.13294518], [65.04056318, 124.61742625,  45.78081377]]),
+        # Test when values are added to pel_origin, pel_x, pel_y, pel_z, and vsk[MeanLegLength]
+        ([1, 0, -3], [-5, -3, -6], [4, -1, 2], [3, 8, 2],
+         {'MeanLegLength': 15.0, 'R_AsisToTrocanterMeasure': 0.0,
+             'L_AsisToTrocanterMeasure': 0.0, 'InterAsisDistance': 0.0},
+         [[100.88576753,  97.85280235, 106.39612748], [61.83654463, 110.86920998,  41.31408931]]),
+        # Test when values are added to pel_origin, pel_x, pel_y, pel_z, and vsk[R_AsisToTrocanterMeasure]
+        ([1, 0, -3], [-5, -3, -6], [4, -1, 2], [3, 8, 2],
+         {'MeanLegLength': 0.0, 'R_AsisToTrocanterMeasure': -24.0,
+             'L_AsisToTrocanterMeasure': 0.0, 'InterAsisDistance': 0.0},
+         [[109.05184203, 109.94699997, 119.13294518], [-57.09307697, 115.44008189,  14.36512267]]),
+        # Test when values are added to pel_origin, pel_x, pel_y, pel_z, and vsk[L_AsisToTrocanterMeasure]
+        ([1, 0, -3], [-5, -3, -6], [4, -1, 2], [3, 8, 2],
+         {'MeanLegLength': 0.0, 'R_AsisToTrocanterMeasure': 0.0,
+             'L_AsisToTrocanterMeasure': 0-7.0, 'InterAsisDistance': 0.0},
+         [[73.42953032, 107.27027453, 109.97003528], [65.04056318, 124.61742625,  45.78081377]]),
+        # Test when values are added to pel_origin, pel_x, pel_y, pel_z, and vsk[InterAsisDistance]
+        ([1, 0, -3], [-5, -3, -6], [4, -1, 2], [3, 8, 2],
+         {'MeanLegLength': 0.0, 'R_AsisToTrocanterMeasure': 0.0,
+             'L_AsisToTrocanterMeasure': 0.0, 'InterAsisDistance': 11.0},
+         [[125.55184203, 104.44699997, 146.63294518], [48.54056318, 130.11742625,  18.28081377]]),
+        # Test when values are added to pel_origin, pel_x, pel_y, pel_z, and all values in vsk
+        ([1, 0, -3], [-5, -3, -6], [4, -1, 2], [3, 8, 2],
+         {'MeanLegLength': 15.0, 'R_AsisToTrocanterMeasure': -24.0,
+             'L_AsisToTrocanterMeasure': -7.0, 'InterAsisDistance': 11.0},
+         [[81.76345582,  89.67607691, 124.73321758], [-76.79709552, 107.19186562, -17.60160178]]),
+        # Testing that when pel_origin, pel_x, pel_y, and pel_z are lists of ints and vsk values are ints
+        ([1, 0, -3], [-5, -3, -6], [4, -1, 2], [3, 8, 2],
+         {'MeanLegLength': 15, 'R_AsisToTrocanterMeasure': -24,
+             'L_AsisToTrocanterMeasure': -7, 'InterAsisDistance': 11},
+         [[81.76345582, 89.67607691, 124.73321758], [-76.79709552, 107.19186562, -17.60160178]]),
+        # Testing that when pel_origin, pel_x, pel_y, and pel_z are numpy arrays of ints and vsk values are ints
+        (np.array([1, 0, -3], dtype='int'), np.array([-5, -3, -6], dtype='int'), np.array([4, -1, 2], dtype='int'),
+         np.array([3, 8, 2], dtype='int'),
+         {'MeanLegLength': 15, 'R_AsisToTrocanterMeasure': -24,
+             'L_AsisToTrocanterMeasure': -7, 'InterAsisDistance': 11},
+         [[81.76345582, 89.67607691, 124.73321758], [-76.79709552, 107.19186562, -17.60160178]]),
+        # Testing that when pel_origin, pel_x, pel_y, and pel_z are lists of floats and vsk values are floats
+        ([1.0, 0.0, -3.0], [-5.0, -3.0, -6.0], [4.0, -1.0, 2.0], [3.0, 8.0, 2.0],
+         {'MeanLegLength': 15.0, 'R_AsisToTrocanterMeasure': -24.0,
+             'L_AsisToTrocanterMeasure': -7.0, 'InterAsisDistance': 11.0},
+         [[81.76345582, 89.67607691, 124.73321758], [-76.79709552, 107.19186562, -17.60160178]]),
+        # Testing that when pel_origin, pel_x, pel_y, and pel_z are numpy arrays of floats and vsk values are floats
+        (np.array([1.0, 0.0, -3.0], dtype='float'), np.array([-5.0, -3.0, -6.0], dtype='float'),
+         np.array([4.0, -1.0, 2.0],
+                  dtype='float'), np.array([3.0, 8.0, 2.0], dtype='float'),
+         {'MeanLegLength': 15.0, 'R_AsisToTrocanterMeasure': -24.0,
+             'L_AsisToTrocanterMeasure': -7.0, 'InterAsisDistance': 11},
+         [[81.76345582, 89.67607691, 124.73321758], [-76.79709552, 107.19186562, -17.60160178]])])
+    def test_hip_joint_center(self, pel_origin, pel_x, pel_y, pel_z, vsk, expected):
+        """
+        This test provides coverage of the hipJointCenter function in pyCGM.py, defined as hipJointCenter(frame, pel_origin, pel_x, pel_y, pel_z, vsk)
+
+        This test takes 6 parameters:
+        pel_origin: array of x,y,z position of origin of the pelvis
+        pel_x: array of x,y,z position of x-axis of the pelvis
+        pel_y: array of x,y,z position of y-axis of the pelvis
+        pel_z: array of x,y,z position of z-axis of the pelvis
+        vsk: dictionary containing subject measurements from a VSK file
+        expected: the expected result from calling hipJointCenter on pel_origin, pel_x, pel_y, pel_z, and vsk
+
+        This test is checking to make sure the hip joint center is calculated correctly given the input parameters.
+        The test checks to see that the correct values in expected are updated per each input parameter added. Any
+        parameter that is added should change the value of every value in expected.
+
+        The hip joint center axis and origin are calculated using the Hip Joint Center Calculation (ref. Davis_1991).
+
+        Lastly, it checks that the resulting output is correct when pel_origin, pel_x, pel_y, and pel_z are composed of
+        lists of ints, numpy arrays of ints, lists of floats, and numpy arrays of floats and vsk values are ints or floats.
+        """
+        pelvis_axis = np.zeros((4, 4))
+        pelvis_axis[3, 3] = 1.0
+        pelvis_axis[0, :3] = np.subtract(pel_x, pel_origin)
+        pelvis_axis[1, :3] = np.subtract(pel_y, pel_origin)
+        pelvis_axis[2, :3] = np.subtract(pel_z, pel_origin)
+        pelvis_axis[:3, 3] = pel_origin
+
+        result = axis.hip_joint_center(pelvis_axis, vsk)
+        print(pelvis_axis)
+        print(result)
+        print(expected)
+        np.testing.assert_almost_equal(
+            result[0], expected[0], rounding_precision)
+        np.testing.assert_almost_equal(
+            result[1], expected[1], rounding_precision)
+
+    @pytest.mark.parametrize(["l_hip_jc", "r_hip_jc", "pelvis_axis", "expected"], [
+        # Test from running sample data
+        (np.array([182.57097863, 339.43231855, 935.52900126]), np.array([308.38050472, 322.80342417, 937.98979061]),
+         [np.array([251.60830688, 391.74131775, 1032.89349365]), np.array([[251.74063624, 392.72694721, 1032.78850073], [
+             250.61711554, 391.87232862, 1032.8741063], [251.60295336, 391.84795134, 1033.88777762]]), np.array([231.57849121, 210.25262451, 1052.24969482])],
+         [[245.47574167208043, 331.1178713574418, 936.7593959314677], [[245.60807102843359, 332.10350081526684, 936.6544030111602], [244.48455032769033, 331.2488822330648, 936.7400085831541], [245.47038814489719, 331.22450494659665, 937.7536799036861]]]),
+        # Basic test with zeros for all params
+        (np.array([0, 0, 0]), np.array([0, 0, 0]),
+         [np.array([0, 0, 0]), np.array(
+             [[0, 0, 0], [0, 0, 0], [0, 0, 0]]), np.array(rand_coor)],
+         [[0, 0, 0], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]),
+        # Testing when values are added to l_hip_jc
+        (np.array([1, -3, 2]), np.array([0, 0, 0]),
+         [np.array([0, 0, 0]), np.array(
+             [[0, 0, 0], [0, 0, 0], [0, 0, 0]]), np.array(rand_coor)],
+         [[0.5, -1.5, 1], [[0.5, -1.5, 1], [0.5, -1.5, 1], [0.5, -1.5, 1]]]),
+        # Testing when values are added to r_hip_jc
+        (np.array([0, 0, 0]), np.array([-8, 1, 4]),
+         [np.array([0, 0, 0]), np.array(
+             [[0, 0, 0], [0, 0, 0], [0, 0, 0]]), np.array(rand_coor)],
+         [[-4, 0.5, 2], [[-4, 0.5, 2], [-4, 0.5, 2], [-4, 0.5, 2]]]),
+        # Testing when values are added to l_hip_jc and r_hip_jc
+        (np.array([8, -3, 7]), np.array([5, -2, -1]),
+         [np.array([0, 0, 0]), np.array(
+             [[0, 0, 0], [0, 0, 0], [0, 0, 0]]), np.array(rand_coor)],
+         [[6.5, -2.5, 3], [[6.5, -2.5, 3], [6.5, -2.5, 3], [6.5, -2.5, 3]]]),
+        # Testing when values are added to pelvis_axis[0]
+        (np.array([0, 0, 0]), np.array([0, 0, 0]),
+         [np.array([1, -3, 6]), np.array([[0, 0, 0], [0, 0, 0],
+                                          [0, 0, 0]]), np.array(rand_coor)],
+         [[0, 0, 0], [[-1, 3, -6], [-1, 3, -6], [-1, 3, -6]]]),
+        # Testing when values are added to pelvis_axis[1]
+        (np.array([0, 0, 0]), np.array([0, 0, 0]),
+         [np.array([0, 0, 0]), np.array(
+             [[1, 0, 5], [-2, -7, -3], [9, -2, 7]]), np.array(rand_coor)],
+         [[0, 0, 0], [[1, 0, 5], [-2, -7, -3], [9, -2, 7]]]),
+        # Testing when values are added to pelvis_axis[0] and pelvis_axis[1]
+        (np.array([0, 0, 0]), np.array([0, 0, 0]),
+         [np.array([-3, 0, 5]), np.array([[-4, 5, -2],
+                                          [0, 0, 0], [8, 5, -1]]), np.array(rand_coor)],
+         [[0, 0, 0], [[-1, 5, -7], [3, 0, -5], [11, 5, -6]]]),
+        # Testing when values are added to all params
+        (np.array([-5, 3, 8]), np.array([-3, -7, -1]),
+         [np.array([6, 3, 9]), np.array(
+             [[5, 4, -2], [0, 0, 0], [7, 2, 3]]), np.array(rand_coor)],
+         [[-4, -2, 3.5], [[-5, -1, -7.5], [-10, -5, -5.5], [-3, -3, -2.5]]]),
+        # Testing that when l_hip_jc, r_hip_jc, and pelvis_axis are composed of lists of ints
+        (np.array([-5, 3, 8]), np.array([-3, -7, -1]),
+         [[6, 3, 9], [[5, 4, -2], [0, 0, 0], [7, 2, 3]], rand_coor],
+         [[-4, -2, 3.5], [[-5, -1, -7.5], [-10, -5, -5.5], [-3, -3, -2.5]]]),
+        # Testing that when l_hip_jc, r_hip_jc, and pelvis_axis are composed of numpy arrays of ints
+        (np.array([-5, 3, 8], dtype='int'), np.array([-3, -7, -1], dtype='int'),
+         [np.array([6, 3, 9], dtype='int'), np.array(
+             [[5, 4, -2], [0, 0, 0], [7, 2, 3]], dtype='int'), rand_coor],
+         [[-4, -2, 3.5], [[-5, -1, -7.5], [-10, -5, -5.5], [-3, -3, -2.5]]]),
+        # Testing that when l_hip_jc, r_hip_jc, and pelvis_axis are composed of lists of floats
+        (np.array([-5.0, 3.0, 8.0]), np.array([-3.0, -7.0, -1.0]),
+         [[6.0, 3.0, 9.0], [[5.0, 4.0, -2.0],
+                            [0.0, 0.0, 0.0], [7.0, 2.0, 3.0]], rand_coor],
+         [[-4, -2, 3.5], [[-5, -1, -7.5], [-10, -5, -5.5], [-3, -3, -2.5]]]),
+        # Testing that when l_hip_jc, r_hip_jc, and pelvis_axis are composed of numpy arrays of floats
+        (np.array([-5.0, 3.0, 8.0], dtype='float'), np.array([-3.0, -7.0, -1.0], dtype='float'),
+         [np.array([6.0, 3.0, 9.0], dtype='float'), np.array(
+             [[5.0, 4.0, -2.0], [0.0, 0.0, 0.0], [7.0, 2.0, 3.0]], dtype='float'), rand_coor],
+         [[-4, -2, 3.5], [[-5, -1, -7.5], [-10, -5, -5.5], [-3, -3, -2.5]]])])
+    def test_hip_axis(_, l_hip_jc, r_hip_jc, pelvis_axis, expected):
+        """
+        This test provides coverage of the hipAxisCenter function in pyCGM.py, defined as hipAxisCenter(l_hip_jc, r_hip_jc, pelvis_axis)
+
+        This test takes 4 parameters:
+        l_hip_jc: array of left hip joint center x,y,z position
+        r_hip_jc: array of right hip joint center x,y,z position
+        pelvis_axis: array of pelvis origin and axis
+        expected: the expected result from calling hipAxisCenter on l_hip_jc, r_hip_jc, and pelvis_axis
+
+        This test is checking to make sure the hip axis center is calculated correctly given the input parameters.
+
+        The hip axis center is calculated using the midpoint of the right and left hip joint centers.
+        Then, the given pelvis_axis variable is converted into x,y,z axis format.
+        The pelvis axis is then translated to the shared hip center by calculating the sum of:
+        pelvis_axis axis component + hip_axis_center axis component
+        """
+        origin, pelvis_axis, _ = pelvis_axis
+
+        pelvis = np.zeros((4, 4))
+        pelvis[3, 3] = 1.0
+        pelvis[0, :3] = np.subtract(pelvis_axis[0], origin)
+        pelvis[1, :3] = np.subtract(pelvis_axis[1], origin)
+        pelvis[2, :3] = np.subtract(pelvis_axis[2], origin)
+        pelvis[:3, 3] = origin
+
+        result = axis.hip_axis(l_hip_jc, r_hip_jc, pelvis)
+
+        expected_pelvis = np.zeros((4, 4))
+        expected_pelvis[3, 3] = 1.0
+        expected_pelvis[0, :3] = np.subtract(expected[1][0], expected[0])
+        expected_pelvis[1, :3] = np.subtract(expected[1][1], expected[0])
+        expected_pelvis[2, :3] = np.subtract(expected[1][2], expected[0])
+        expected_pelvis[:3, 3] = expected[0]
+
+        np.testing.assert_almost_equal(
+            result, expected_pelvis, rounding_precision
+        )
