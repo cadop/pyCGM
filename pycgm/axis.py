c85ae4a2684ff75ddb662f38996624bca44035db
AmeerYHassan <ayh7@njit.edu>
2021-04-22 15:58:17 -0400

    added latex

--- pycgm/axis.py
+++ pycgm/axis.py
@@ -2,6 +2,7 @@ import math
 from math import pi
 import numpy as np
 
+
 def rotmat(x=0, y=0, z=0):
     r"""Rotation Matrix.
 
@@ -43,28 +44,35 @@ def rotmat(x=0, y=0, z=0):
     [-0.02,  0.02,  1.  ]])
     """
     x, y, z = math.radians(x), math.radians(y), math.radians(z)
-    r_x = [ [1,0,0],[0,math.cos(x),math.sin(x)*-1],[0,math.sin(x),math.cos(x)] ]
-    r_y = [ [math.cos(y),0,math.sin(y)],[0,1,0],[math.sin(y)*-1,0,math.cos(y)] ]
-    r_z = [ [math.cos(z),math.sin(z)*-1,0],[math.sin(z),math.cos(z),0],[0,0,1] ]
-    r_xy = np.matmul(r_x,r_y)
-    r_xyz = np.matmul(r_xy,r_z)
+    r_x = [[1, 0, 0], [0, math.cos(x), math.sin(x) * -1], [0, math.sin(x), math.cos(x)]]
+    r_y = [[math.cos(y), 0, math.sin(y)], [0, 1, 0], [math.sin(y) * -1, 0, math.cos(y)]]
+    r_z = [[math.cos(z), math.sin(z) * -1, 0], [math.sin(z), math.cos(z), 0], [0, 0, 1]]
+    r_xy = np.matmul(r_x, r_y)
+    r_xyz = np.matmul(r_xy, r_z)
 
     return r_xyz
 
-def get_shoulder_angle(axisP,axisD):
-    """Shoulder angle calculation.
+
+def get_shoulder_angle(axis_p, axis_d):
+    r"""Shoulder angle calculation.
 
     This function takes in two axes and returns three angles and uses the
     inverse Euler rotation matrix in YXZ order.
 
     Returns the angles in degrees.
+    
+    :math:`\alpha = \arcsin{(axis\_d_{z} \cdot axis\_p_{x})}`
+
+    :math:`\beta = \arctan2{(-(axis\_d_{z} \cdot axis\_p_{y}), axis\_d_{z} \cdot axis\_p_{z})}`
+
+    :math:`\gamma = \arctan2{(-(axis\_d_{y} \cdot axis\_p_{x}), axis\_d_{x} \cdot axis\_p_{x})}`
 
     Parameters
     ----------
-    axisP : list
-        Shows the unit vector of axisP, the position of the proximal axis.
-    axisD : list
-        Shows the unit vector of axisD, the position of the distal axis.
+    axis_p : list
+        Shows the unit vector of axis_p, the position of the proximal axis.
+    axis_d : list
+        Shows the unit vector of axis_d, the position of the distal axis.
 
     Returns
     -------
@@ -75,13 +83,13 @@ def get_shoulder_angle(axisP,axisD):
     --------
     >>> import numpy as np
     >>> from .axis import get_shoulder_angle
-    >>> axisP = [[ 0.04, 0.99, 0.06],
+    >>> axis_p = [[ 0.04, 0.99, 0.06],
     ...        [ 0.99, -0.04, -0.05],
     ...       [-0.05,  0.07, -0.99]]
-    >>> axisD = [[-0.18, -0.98, -0.02],
+    >>> axis_d = [[-0.18, -0.98, -0.02],
     ...        [ 0.71, -0.11, -0.69],
     ...        [ 0.67, -0.14, 0.72 ]]
-    >>> np.around(get_shoulder_angle(axisP,axisD), 2)
+    >>> np.around(get_shoulder_angle(axis_p,axis_d), 2)
     array([  -3.93, -140.07,  172.9 ])
     """
 
@@ -90,10 +98,34 @@ def get_shoulder_angle(axisP,axisD):
     # alpha is internal / external rotation
 
     # this is shoulder angle calculation
-    alpha = np.arcsin(((axisD[2][0]*axisP[0][0])+(axisD[2][1]*axisP[0][1])+(axisD[2][2]*axisP[0][2])))
-    beta = np.arctan2(-1*((axisD[2][0]*axisP[1][0])+(axisD[2][1]*axisP[1][1])+(axisD[2][2]*axisP[1][2])) , ((axisD[2][0]*axisP[2][0])+(axisD[2][1]*axisP[2][1])+(axisD[2][2]*axisP[2][2])))
-    gamma = np.arctan2(-1*((axisD[1][0]*axisP[0][0])+(axisD[1][1]*axisP[0][1])+(axisD[1][2]*axisP[0][2])) , ((axisD[0][0]*axisP[0][0])+(axisD[0][1]*axisP[0][1])+(axisD[0][2]*axisP[0][2])))
+    alpha = np.arcsin(
+            (axis_d[2][0] * axis_p[0][0])
+            + (axis_d[2][1] * axis_p[0][1])
+            + (axis_d[2][2] * axis_p[0][2])
+    )
+    beta = np.arctan2(
+        -1 * (
+            (axis_d[2][0] * axis_p[1][0])
+            + (axis_d[2][1] * axis_p[1][1])
+            + (axis_d[2][2] * axis_p[1][2])
+        ), (
+            (axis_d[2][0] * axis_p[2][0])
+            + (axis_d[2][1] * axis_p[2][1])
+            + (axis_d[2][2] * axis_p[2][2])
+        )
+    )
+    gamma = np.arctan2(
+        -1 * (
+            (axis_d[1][0] * axis_p[0][0])
+            + (axis_d[1][1] * axis_p[0][1])
+            + (axis_d[1][2] * axis_p[0][2])
+        ), (
+            (axis_d[0][0] * axis_p[0][0])
+            + (axis_d[0][1] * axis_p[0][1])
+            + (axis_d[0][2] * axis_p[0][2])
+        ),
+    )
 
-    angle = [180.0 * alpha/ pi, 180.0 *beta/ pi, 180.0 * gamma/ pi]
+    angle = [180.0 * alpha / pi, 180.0 * beta / pi, 180.0 * gamma / pi]
 
     return angle
--- pycgm/tests/test_axis_angle.py
+++ pycgm/tests/test_axis_angle.py
@@ -18,64 +18,73 @@ class TestPycgmAngle():
     @pytest.mark.parametrize(["xRot", "yRot", "zRot", "expected"], [
         (0, 0, 0, [0, 0, 0]),
         # X rotations
-        (90, 0, 0, [0, 0, 90]), (30, 0, 0, [0, 0, 30]), (-30, 0, 0, [0, 0, -30]), (120, 0, 0, [0, 0, 60]), (-120, 0, 0, [0, 0, -60]), (180, 0, 0, [0, 0, 0]),
+        (90, 0, 0, [0, 90, 0]), (30, 0, 0, [0, 30, 0]), (-30, 0, 0, [0, -30, 0]), (120, 0, 0, [0, 120, 0]),
+        (-120, 0, 0, [0, -120, 0]), (180, 0, 0, [0, 180, 0]),
         # Y rotations
-        (0, 90, 0, [90, 0, 0]), (0, 30, 0, [30, 0, 0]), (0, -30, 0, [-30, 0, 0]), (0, 120, 0, [60, 0, 0]), (0, -120, 0, [-60, 0, 0]), (0, 180, 0, [0, 0, 0]),
+        (0, 90, 0, [90, 0, 0]), (0, 30, 0, [30, 0, 0]), (0, -30, 0, [-30, 0, 0]), (0, 120, 0, [60, -180, -180]),
+        (0, -120, 0, [-60, -180, -180]), (0, 180, 0, [0, -180, -180]),
         # Z rotations
-        (0, 0, 90, [0, 90, 0]), (0, 0, 30, [0, 30, 0]), (0, 0, -30, [0, -30, 0]), (0, 0, 120, [0, 60, 0]), (0, 0, -120, [0, -60, 0]), (0, 0, 180, [0, 0, 0]),
+        (0, 0, 90, [0, 0, 90]), (0, 0, 30, [0, 0, 30]), (0, 0, -30, [0, 0, -30]), (0, 0, 120, [0, 0, 120]),
+        (0, 0, -120, [0, 0, -120]), (0, 0, 180, [0, 0, 180]),
         # Multiple Rotations
-        (150, 30, 0, [-30, 0, 30]), (45, 0, 60, [-40.89339465, 67.7923457, 20.70481105]), (0, 90, 120, [-90, 0, 60]), (135, 45, 90, [-54.73561032, 54.73561032, -30])
+        (150, 30, 0, [30, 150, 0]), (45, 0, 60, [0, 45, 60]), (0, 90, 120, [90, 0, 120]), (135, 45, 90, [45, 135, 90])
     ])
-    def test_get_spine_angle(self, xRot, yRot, zRot, expected):
+    def test_get_shoulder_angle(self, xRot, yRot, zRot, expected):
         """
-        This test provides coverage of the get_spine_angle function in axis.py,
-        defined as get_spine_angle(axisP,axisD) where axisP is the proximal axis and axisD is the distal axis
-        get_spine_angle takes in as input two axes, axisP and axisD, and returns in degrees, the Euler angle
-        rotations required to rotate axisP to axisD as a list [beta, gamma, alpha]. get_spine_angle uses the XZX
-        order of Euler rotations to calculate the angles. The rotation matrix is obtained by directly comparing
+        This test provides coverage of the get_shoulder_angle function in axis.py,
+        defined as get_shoulder_angle(axisP,axisD) where axisP is the proximal axis and axisD is the distal axis.
+
+        get_shoulder_angle takes in as input two axes, axisP and axisD, and returns in degrees, the Euler angle
+        rotations required to rotate axisP to axisD as a list [alpha, beta, gamma]. get_shoulder_angle uses the XYZ
+        order Euler rotations to calculate the angles. The rotation matrix is obtained by directly comparing
         the vectors in axisP to those in axisD through dot products between different components
         of each axis. axisP and axisD each have 3 components to their axis, x, y, and z. 
         The angles are calculated as follows:
+
         .. math::
-            \[ alpha = \arcsin{(axisD_{y} \cdot axisP_{z})} \]
-            \[ gamma = \arcsin{(-(axisD_{y} \cdot axisP_{x}) / \cos{\alpha})} \]
-            \[ beta = \arcsin{(-(axisD_{x} \cdot axisP_{z}) / \cos{\alpha})} \]
+            \[ \alpha = \arcsin{(axisD_{z} \cdot axisP_{x})} \]
+
+            \[ \beta = \arctan2{(-(axisD_{z} \cdot axisP_{y}), axisD_{z} \cdot axisP_{z})} \]
+
+            \[ \gamma = \arctan2{(-(axisD_{y} \cdot axisP_{x}), axisD_{x} \cdot axisP_{x})} \]
+
         This test calls axis.rotmat() to create axisP with an x, y, and z rotation defined in the parameters.
-        It then calls axis.get_spine_angle() with axisP and axisD, which was created with no rotation in the
+        It then calls axis.get_shoulder_angle() with axisP and axisD, which was created with no rotation in the
         x, y or z direction. This result is then compared to the expected result. The results from this test will
-        be in the YZX order, meaning that a parameter with an inputed x rotation will have a result with the same
-        angle in the z direction. The only exception to this is a 120, -120, or 180 degree Y rotation. The exception
-        to this is that 120, -120, and 180 degree rotations end up with 60, -60, and 0 degree angles respectively.
+        be in the YXZ order, meaning that a parameter with an inputed x rotation will have a result with the same
+        angle in the y direction. The only exception to this is a 120, -120, or 180 degree Y rotation. These will end
+        up with a 60, -60, and 0 degree angle in the X direction respectively, and with a -180 degree
+        angle in the y and z direction.
         """
         # Create axisP as a rotatinal matrix using the x, y, and z rotations given in testcase
         axisP = axis.rotmat(xRot, yRot, zRot)
         axisD = axis.rotmat(0, 0, 0)
-        result = axis.get_spine_angle(axisP, axisD)
+        result = axis.get_shoulder_angle(axisP, axisD)
         np.testing.assert_almost_equal(result, expected, rounding_precision)
 
-    def test_get_spine_angle_datatypes(self):
+    def test_get_shoulder_angle_datatypes(self):
         """
-        This test provides coverage of the get_spine_angle function in axis.py, defined as get_spine_angle(axisP,axisD).
-        It checks that the resulting output from calling get_spine_angle is correct for a list of ints, a numpy array of
+        This test provides coverage of the get_shoulder_angle function in axis.py, defined as get_shoulder_angle(axisP,axisD).
+        It checks that the resulting output from calling get_shoulder_angle is correct for a list of ints, a numpy array of
         ints, a list of floats, and a numpy array of floats.
         """
         axisD = axis.rotmat(0, 0, 0)
         axisP_floats = axis.rotmat(90, 0, 90)
         axisP_ints = [[int(y) for y in x] for x in axisP_floats]
-        expected = [-90, 90, 0]
+        expected = [0, 90, 90]
 
-        # Check that calling get_spine_angle on a list of ints yields the expected results
-        result_int_list = axis.get_spine_angle(axisP_ints, axisD)
+        # Check that calling get_shoulder_angle on a list of ints yields the expected results
+        result_int_list = axis.get_shoulder_angle(axisP_ints, axisD)
         np.testing.assert_almost_equal(result_int_list, expected, rounding_precision)
 
-        # Check that calling get_spine_angle on a numpy array of ints yields the expected results
-        result_int_nparray = axis.get_spine_angle(np.array(axisP_ints, dtype='int'), np.array(axisD, dtype='int'))
+        # Check that calling get_shoulder_angle on a numpy array of ints yields the expected results
+        result_int_nparray = axis.get_shoulder_angle(np.array(axisP_ints, dtype='int'), np.array(axisD, dtype='int'))
         np.testing.assert_almost_equal(result_int_nparray, expected, rounding_precision)
 
-        # Check that calling get_spine_angle on a list of floats yields the expected results
-        result_float_list = axis.get_spine_angle(axisP_floats, axisD)
+        # Check that calling get_shoulder_angle on a list of floats yields the expected results
+        result_float_list = axis.get_shoulder_angle(axisP_floats, axisD)
         np.testing.assert_almost_equal(result_float_list, expected, rounding_precision)
 
-        # Check that calling get_spine_angle on a numpy array of floats yields the expected results
-        result_float_nparray = axis.get_spine_angle(np.array(axisP_floats, dtype='float'), np.array(axisD, dtype='float'))
+        # Check that calling get_shoulder_angle on a numpy array of floats yields the expected results
+        result_float_nparray = axis.get_shoulder_angle(np.array(axisP_floats, dtype='float'), np.array(axisD, dtype='float'))
         np.testing.assert_almost_equal(result_float_nparray, expected, rounding_precision)
