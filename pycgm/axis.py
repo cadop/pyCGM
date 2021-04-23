5e77a04dd2a0096eebef80f216b3d93eb59eaa33
AmeerYHassan <ayh7@njit.edu>
2021-04-22 23:46:35 -0400

    added tests to head function

--- pycgm/axis.py
+++ pycgm/axis.py
@@ -53,19 +53,13 @@ def rotmat(x=0, y=0, z=0):
     return r_xyz
 
 
-def get_shoulder_angle(axis_p, axis_d):
-    r"""Shoulder angle calculation.
+def get_head_angle(axis_p, axis_d):
+    """Head angle calculation function.
 
     This function takes in two axes and returns three angles and uses the
     inverse Euler rotation matrix in YXZ order.
 
     Returns the angles in degrees.
-
-    :math:`\alpha = \arcsin{(axis\_d_{z} \cdot axis\_p_{x})}`
-
-    :math:`\beta = \arctan2{(-(axis\_d_{z} \cdot axis\_p_{y}), axis\_d_{z} \cdot axis\_p_{z})}`
-
-    :math:`\gamma = \arctan2{(-(axis\_d_{y} \cdot axis\_p_{x}), axis\_d_{x} \cdot axis\_p_{x})}`
 
     Parameters
     ----------
@@ -82,33 +76,55 @@ def get_shoulder_angle(axis_p, axis_d):
     Examples
     --------
     >>> import numpy as np
-    >>> from .axis import get_shoulder_angle
+    >>> from .axis import get_head_angle
     >>> axis_p = [[ 0.04, 0.99, 0.06],
     ...        [ 0.99, -0.04, -0.05],
     ...       [-0.05,  0.07, -0.99]]
     >>> axis_d = [[-0.18, -0.98, -0.02],
     ...        [ 0.71, -0.11, -0.69],
     ...        [ 0.67, -0.14, 0.72 ]]
-    >>> np.around(get_shoulder_angle(axis_p,axis_d), 2)
-    array([  -3.93, -140.07,  172.9 ])
+    >>> np.around(get_head_angle(axis_p,axis_d), 2)
+    array([ 185.18,  -39.99, -190.54])
     """
+    # this is the angle calculation which order is Y-X-Z
+    # alpha is abdcution angle.
 
-    # beta is flexion / extension
-    # gamma is adduction / abduction
-    # alpha is internal / external rotation
+    ang = (
+        (-1 * axis_d[2][0] * axis_p[1][0])
+        + (-1 * axis_d[2][1] * axis_p[1][1])
+        + (-1 * axis_d[2][2] * axis_p[1][2])
+    )
+    alpha = np.nan
+    if -1 <= ang <= 1:
+        alpha = np.arcsin(ang)
 
-    # this is shoulder angle calculation
-    alpha = np.arcsin(
+    # check the abduction angle is in the area between -pi/2 and pi/2
+    # beta is flextion angle
+    # gamma is rotation angle
+
+    beta = np.arctan2(
+            (axis_d[2][0] * axis_p[1][0])
+            + (axis_d[2][1] * axis_p[1][1])
+            + (axis_d[2][2] * axis_p[1][2]),
+        np.sqrt(
+            (
+                axis_d[0][0] * axis_p[1][0]
+                + axis_d[0][1] * axis_p[1][1]
+                + axis_d[0][2] * axis_p[1][2]
+            ) ** 2
+            + (
+                axis_d[1][0] * axis_p[1][0]
+                + axis_d[1][1] * axis_p[1][1]
+                + axis_d[1][2] * axis_p[1][2]
+            ) ** 2
+        ),
+    )
+
+    alpha = np.arctan2(
+        -1 * (
             (axis_d[2][0] * axis_p[0][0])
             + (axis_d[2][1] * axis_p[0][1])
             + (axis_d[2][2] * axis_p[0][2])
-    )
-
-    beta = np.arctan2(
-        -1 * (
-            (axis_d[2][0] * axis_p[1][0])
-            + (axis_d[2][1] * axis_p[1][1])
-            + (axis_d[2][2] * axis_p[1][2])
         ), (
             (axis_d[2][0] * axis_p[2][0])
             + (axis_d[2][1] * axis_p[2][1])
@@ -118,16 +134,40 @@ def get_shoulder_angle(axis_p, axis_d):
 
     gamma = np.arctan2(
         -1 * (
-            (axis_d[1][0] * axis_p[0][0])
-            + (axis_d[1][1] * axis_p[0][1])
-            + (axis_d[1][2] * axis_p[0][2])
+            (axis_d[0][0] * axis_p[1][0])
+            + (axis_d[0][1] * axis_p[1][1])
+            + (axis_d[0][2] * axis_p[1][2])
         ), (
-            (axis_d[0][0] * axis_p[0][0])
-            + (axis_d[0][1] * axis_p[0][1])
-            + (axis_d[0][2] * axis_p[0][2])
+            (axis_d[1][0] * axis_p[1][0])
+            + (axis_d[1][1] * axis_p[1][1])
+            + (axis_d[1][2] * axis_p[1][2])
         ),
     )
 
-    angle = [180.0 * alpha / pi, 180.0 * beta / pi, 180.0 * gamma / pi]
+    alpha = 180.0 * alpha / pi
+    beta = 180.0 * beta / pi
+    gamma = 180.0 * gamma / pi
+
+    beta *= -1
+
+    if alpha < 0:
+        alpha *= -1
+    else:
+        if 0 < alpha < 180:
+            alpha = 180 + (180 - alpha)
+
+    if gamma > 90.0:
+        if gamma > 120:
+            gamma = (gamma - 180) * -1
+        else:
+            gamma = (gamma + 180) * -1
+
+    else:
+        if gamma < 0:
+            gamma = (gamma + 180) * -1
+        else:
+            gamma = (gamma * -1) - 180.0
+
+    angle = [alpha, beta, gamma]
 
     return angle
