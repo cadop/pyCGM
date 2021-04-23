6511f4e071594a39ba8ebc301e00ed584ba14cdf
AmeerYHassan <ayh7@njit.edu>
2021-04-23 15:59:39 -0400

    refactored get_pelvis_angle function

--- pycgm/tests/test_axis_angle.py
+++ pycgm/tests/test_axis_angle.py
@@ -12,76 +12,76 @@ class TestPycgmAngle():
     get_angle_spi
     get_angle
     get_head_angle
-    getPelangle
+    get_pelvis_angle
     """
 
     @pytest.mark.parametrize(["xRot", "yRot", "zRot", "expected"], [
-        (0, 0, 0, [0, 0, -180]),
+        (0, 0, 0, [0, 0, 0]),
         # X rotations
-        (90, 0, 0, [0, 90, -180]), (30, 0, 0, [0, 30, -180]), (-30, 0, 0, [0, -30, -180]), (120, 0, 0, [180, 60, 0]), (-120, 0, 0, [180, -60, 0]), (180, 0, 0, [180, 0, 0]),
+        (90, 0, 0, [0, -90, 0]), (30, 0, 0, [0, -30, 0]), (-30, 0, 0, [0, 30, 0]), (120, 0, 0, [180, -60, 180]), (-120, 0, 0, [180, 60, 180]), (180, 0, 0, [180, 0, 180]),
         # Y rotations
-        (0, 90, 0, [90, 0, -180]), (0, 30, 0, [30, 0, -180]), (0, -30, 0, [330, 0, -180]), (0, 120, 0, [120, 0, -180]), (0, -120, 0, [240, 0, -180]), (0, 180, 0, [180, 0, -180]),
+        (0, 90, 0, [90, 0, 0]), (0, 30, 0, [30, 0, 0]), (0, -30, 0, [-30, 0, 0]), (0, 120, 0, [120, 0, 0]), (0, -120, 0, [-120, 0, -0]), (0, 180, 0, [180, 0, 0]),
         # Z rotations
-        (0, 0, 90, [0, 0, -90]), (0, 0, 30, [0, 0, -150]), (0, 0, -30, [0, 0, -210]), (0, 0, 120, [0, 0, -60]), (0, 0, -120, [0, 0, -300]), (0, 0, 180, [0, 0, 0]),
+        (0, 0, 90, [0, 0, 90]), (0, 0, 30, [0, 0, 30]), (0, 0, -30, [0, 0, -30]), (0, 0, 120, [0, 0, 120]), (0, 0, -120, [0, 0, -120]), (0, 0, 180, [0, 0, 180]),
         # Multiple Rotations
-        (150, 30, 0, [146.30993247, 25.65890627, -16.10211375]), (45, 0, 60, [0, 45, -120]), (0, 90, 120, [90, 0, -60]), (135, 45, 90, [125.26438968, 30, 54.73561032])
+        (150, 30, 0, [146.30993247, -25.65890627, 163.89788625]), (45, 0, 60, [0, -45, 60]), (0, 90, 120, [90, 0, 120]), (135, 45, 90, [125.26438968, -30, -125.26438968])
     ])
-    def test_get_head_angle(self, xRot, yRot, zRot, expected):
+    def test_get_pelvis_angle(self, xRot, yRot, zRot, expected):
         """
-        This test provides coverage of the get_head_angle function in axis.py,
-        defined as get_head_angle(axisP,axisD) where axisP is the proximal axis and axisD is the distal axis
+        This test provides coverage of the get_pelvis_angle function in axis.py,
+        defined as get_pelvis_angle(axisP,axisD) where axisP is the proximal axis and axisD is the distal axis
 
-        get_head_angle takes in as input two axes, axisP and axisD, and returns in degrees, the Euler angle
-        rotations required to rotate axisP to axisD as a list [alpha, beta, gamma]. get_head_angle uses the YXZ
+        get_pelvis_angle takes in as input two axes, axisP and axisD, and returns in degrees, the Euler angle
+        rotations required to rotate axisP to axisD as a list [alpha, beta, gamma]. get_pelvis_angle uses the YXZ
         order of Euler rotations to calculate the angles. The rotation matrix is obtained by directly comparing
         the vectors in axisP to those in axisD through dot products between different components
         of each axis. axisP and axisD each have 3 components to their axis, x, y, and z. 
         The angles are calculated as follows:
 
         .. math::
-            \[ \beta = \arctan2{((axisD_{z} \cdot axisP_{y}), \sqrt{(axisD_{x} \cdot axisP_{y})^2 + (axisD_{y} \cdot axisP_{y})^2}}) \]
+            \[ \beta = \arctan2{((axisD_{z} \cdot axisP_{y}), \sqrt{(axisD_{z} \cdot axisP_{x})^2 + (axisD_{z} \cdot axisP_{z})^2}}) \]
 
-            \[ \alpha = \arctan2{(-(axisD_{z} \cdot axisP_{x}), axisD_{z} \cdot axisP_{z})} \]
+            \[ \alpha = \arctan2{((axisD_{z} \cdot axisP_{x}), axisD_{z} \cdot axisP_{z})} \]
 
-            \[ \gamma = \arctan2{(-(axisD_{x} \cdot axisP_{y}), axisD_{y} \cdot axisP_{y})} \]
+            \[ \gamma = \arctan2{((axisD_{x} \cdot axisP_{y}), axisD_{y} \cdot axisP_{y})} \]
 
         This test calls axis.rotmat() to create axisP with an x, y, and z rotation defined in the parameters.
-        It then calls axis.get_head_angle() with axisP and axisD, which was created with no rotation in the x, y or z
+        It then calls axis.getHeadangle() with axisP and axisD, which was created with no rotation in the x, y or z
         direction. This result is then compared to the expected result. The results from this test will be in the
         YXZ order, meaning that a parameter with an inputed x rotation will have a result with the same angle in
-        the z direction. There is also an additional -180 degree angle in the z direction if there was no z rotation.
-        If there was a z rotation than there will be a different angle in the z direction. A z rotation of 90, 30, -30,
-        120, -120, 180 degrees results in a -90, -150, -210, -60, -300, 0 degree angle in the z direction respectively.
+        the z direction. The exception to this is x rotations. An x rotation of 90, 30, -30, 120, -120, 180
+        degrees results in a -90, -30, 30, -6, 60, 0 degree angle in the y direction respectively. A x rotation or
+        120, -120, or 180 also results in a 180 degree rotation in the x and z angles.
         """
         # Create axisP as a rotatinal matrix using the x, y, and z rotations given in testcase
         axisP = axis.rotmat(xRot, yRot, zRot)
         axisD = axis.rotmat(0, 0, 0)
-        result = axis.get_head_angle(axisP, axisD)
+        result = axis.get_pelvis_angle(axisP, axisD)
         np.testing.assert_almost_equal(result, expected, rounding_precision)
 
-    def test_get_head_angle_datatypes(self):
+    def test_get_pelvis_angle_datatypes(self):
         """
-        This test provides coverage of the get_head_angle function in axis.py, defined as get_head_angle(axisP,axisD).
-        It checks that the resulting output from calling get_head_angle is correct for a list of ints, a numpy array of
+        This test provides coverage of the get_pelvis_angle function in axis.py, defined as get_pelvis_angle(axisP,axisD).
+        It checks that the resulting output from calling get_pelvis_angle is correct for a list of ints, a numpy array of
         ints, a list of floats, and a numpy array of floats.
         """
         axisD = axis.rotmat(0, 0, 0)
         axisP_floats = axis.rotmat(90, 90, 90)
         axisP_ints = [[int(y) for y in x] for x in axisP_floats]
-        expected = [90, 0, 0]
+        expected = [90, 0, 180]
 
-        # Check that calling get_head_angle on a list of ints yields the expected results
-        result_int_list = axis.get_head_angle(axisP_ints, axisD)
+        # Check that calling get_pelvis_angle on a list of ints yields the expected results
+        result_int_list = axis.get_pelvis_angle(axisP_ints, axisD)
         np.testing.assert_almost_equal(result_int_list, expected, rounding_precision)
 
-        # Check that calling get_head_angle on a numpy array of ints yields the expected results
-        result_int_nparray = axis.get_head_angle(np.array(axisP_ints, dtype='int'), np.array(axisD, dtype='int'))
+        # Check that calling get_pelvis_angle on a numpy array of ints yields the expected results
+        result_int_nparray = axis.get_pelvis_angle(np.array(axisP_ints, dtype='int'), np.array(axisD, dtype='int'))
         np.testing.assert_almost_equal(result_int_nparray, expected, rounding_precision)
 
-        # Check that calling get_head_angle on a list of floats yields the expected results
-        result_float_list = axis.get_head_angle(axisP_floats, axisD)
+        # Check that calling get_pelvis_angle on a list of floats yields the expected results
+        result_float_list = axis.get_pelvis_angle(axisP_floats, axisD)
         np.testing.assert_almost_equal(result_float_list, expected, rounding_precision)
 
-        # Check that calling get_head_angle on a numpy array of floats yields the expected results
-        result_float_nparray = axis.get_head_angle(np.array(axisP_floats, dtype='float'), np.array(axisD, dtype='float'))
+        # Check that calling get_pelvis_angle on a numpy array of floats yields the expected results
+        result_float_nparray = axis.get_pelvis_angle(np.array(axisP_floats, dtype='float'), np.array(axisD, dtype='float'))
         np.testing.assert_almost_equal(result_float_nparray, expected, rounding_precision)
