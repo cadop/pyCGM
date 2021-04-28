ab7af17cbe4775ab4557edf562ebc465c1badd56
khgaw <kgaw2017@gmail.com>
2021-04-27 23:12:03 -0400

    array.array error

--- pycgm/temp.py
+++ pycgm/temp.py
@@ -1,13 +1,14 @@
 """
 This file is used in coordinate and vector calculations.
-
 """
+
 # pyCGM
 
 # This module was contributed by Neil M. Thomas
 # the CoM calculation is not an exact clone of PiG,
 # but the differences are not clinically significant.
 # We will add updates accordingly.
+#
 
 from __future__ import division
 import os
@@ -19,8 +20,227 @@ if sys.version_info[0] == 2:
 else:
     pyver = 3
 
+# helper functions useful for dealing with frames of data, i.e. 1d arrays of (x,y,z)
+# coordinate. Also in Utilities but need to clean everything up somewhat!
 
-def pnt_line(pnt, start, end):
+
+def f(p, x):
+    """ Helper function for working with frames of data.
+
+    Parameters
+    ----------
+    p : list
+        A list of at least length 2.
+    x : int or float
+        Scaling factor.
+
+    Returns
+    -------
+    int or float
+        Returns the first value in `p` scaled by `x`, added by the second value in `p`.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from .pycgmKinetics import f
+    >>> p = [1, 2]
+    >>> x = 10
+    >>> f(p, x)
+    12
+    """
+    return (p[0] * x) + p[1]
+
+
+def dot(v, w):
+    """Calculate dot product of two points.
+
+    Parameters
+    ----------
+    v : list
+        An (x, y, z) coordinate.
+    w : list
+        An (x, y, z) coordinate.
+
+    Returns
+    -------
+    int or float
+        Returns the dot product of vectors `v` and `w`.
+
+    Examples
+    --------
+    >>> from .pycgmKinetics import dot
+    >>> v = [1, 2, 3]
+    >>> w = [4, 5, 6]
+    >>> dot(v,w)
+    32
+    """
+    x, y, z = v
+    X, Y, Z = w
+    return x*X + y*Y + z*Z
+
+
+def length(v):
+    """Calculate length of a 3D vector.
+
+    Parameters
+    ----------
+    v : list
+        A 3D vector.
+
+    Returns
+    -------
+    float
+        Returns the length of `v`.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from .pycgmKinetics import length
+    >>> v = [1,2,3]
+    >>> np.around(length(v), 2)
+    3.74
+    """
+    x, y, z = v
+    return np.sqrt(x*x + y*y + z*z)
+
+
+def vector(b, e):
+    """Subtracts two vectors.
+
+    Parameters
+    ----------
+    v : list
+        First 3D vector.
+    e : list
+        Second 3D vector.
+
+    Returns
+    -------
+    tuple
+        Returns the vector `e` - `v`.
+
+    Examples
+    --------
+    >>> from .pycgmKinetics import vector
+    >>> v = [1,2,3]
+    >>> e = [4,5,6]
+    >>> vector(v, e)
+    (3, 3, 3)
+    """
+    x, y, z = b
+    X, Y, Z = e
+    return (X-x, Y-y, Z-z)
+
+
+def unit(v):
+    """Calculate unit vector.
+
+    Parameters
+    ----------
+    v : list
+        A 3D vector.
+
+    Returns
+    -------
+    tuple
+        Returns the unit vector of `v`.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from .pycgmKinetics import unit
+    >>> v = [1,2,3]
+    >>> np.around(unit(v), 2)
+    array([0.27, 0.53, 0.8 ])
+    """
+    x, y, z = v
+    mag = length(v)
+    return (x/mag, y/mag, z/mag)
+
+
+def distance(p0, p1):
+    """Calculate distance between two points
+
+    Parameters
+    ----------
+    p0 : list
+        An x, y, z coordinate point.
+    p1 : list
+        An x, y, z coordinate point.
+
+    Returns
+    -------
+    float
+        Returns distance between `p0` and `p1`.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from .pycgmKinetics import distance
+    >>> p0 = [1,2,3]
+    >>> p1 = [4,5,6]
+    >>> np.around(distance(p0,p1), 2)
+    5.2
+    """
+    return length(vector(p0, p1))
+
+
+def scale(v, sc):
+    """Scale a vector.
+
+    Parameters
+    ----------
+    v : list
+        A 3D vector.
+    sc : int or float
+        A scaling factor.
+
+    Returns
+    -------
+    tuple
+        Returns `v` scaled by `sc`.
+
+    Examples
+    --------
+    >>> from .pycgmKinetics import scale
+    >>> v = [1,2,3]
+    >>> sc = 2
+    >>> scale(v, sc)
+    (2, 4, 6)
+    """
+    x, y, z = v
+    return (x * sc, y * sc, z * sc)
+
+
+def add(v, w):
+    """Add two vectors.
+
+    Parameters
+    ----------
+    v : list
+        A 3D vector.
+    w : list
+        A 3D vector.
+
+    Returns
+    -------
+    tuple
+        Returns the `v` + `w`.
+
+    Examples
+    --------
+    >>> from .pycgmKinetics import add
+    >>> v = [1, 2, 3]
+    >>> w = [4, 5, 6]
+    >>> add(v, w)
+    (5, 7, 9)
+    """
+    x, y, z = v
+    X, Y, Z = w
+    return (x+X, y+Y, z+Z)
+
+
+def pnt2line(pnt, start, end):
     """Calculate shortest distance between a point and line.
 
     The line is represented by the points `start` and `end`.
@@ -44,45 +264,52 @@ def pnt_line(pnt, start, end):
     Examples
     --------
     >>> import numpy as np
-    >>> from .kinetics import pnt_line
+    >>> from .pycgmKinetics import pnt2line
     >>> pnt = [1, 2, 3]
     >>> start = [4, 2, 3]
     >>> end = [2, 2, 3]
-    >>> pnt_line(pnt, start, end)
-    (2.0, 2.0, 3.0)
+    >>> pnt2line(pnt, start, end)
+    (1.0, (2.0, 2.0, 3.0), [1, 2, 3])
     """
-    line_vec = np.subtract(end, start)
-    pnt_vec = np.subtract(pnt, start)
+    lineVec = vector(start, end)
 
-    line_length = np.sqrt(np.dot(line_vec, line_vec))
+    pntVec = vector(start, pnt)
 
-    line_unit_vec = np.multiply(line_vec, 1/line_length)
-    pnt_vec_scaled = np.multiply(pnt_vec, 1/line_length)
+    lineLength = length(lineVec)
+    lineUnitVec = unit(lineVec)
+    pntVecScaled = scale(pntVec, 1.0/lineLength)
 
-    t = np.dot(line_unit_vec, pnt_vec_scaled)
+    t = dot(lineUnitVec, pntVecScaled)
 
     if t < 0.0:
         t = 0.0
     elif t > 1.0:
         t = 1.0
 
-    nearest = (line_vec[0]*t, line_vec[1]*t, line_vec[2]*t)
-    nearest = tuple(np.add(nearest, start))
+    nearest = scale(lineVec, t)
+    dist = distance(nearest, pntVec)
 
-    return nearest
+    nearest = add(nearest, start)
+
+    return dist, nearest, pnt
 
 
-def find_L5(frame):
-    """Calculate L5 Markers using a thorax or pelvis axis
+# def norm3d(v):
+#    try:
+#        return np.asarray(sqrt((v[0]*v[0]+v[1]*v[1]+v[2]*v[2])))
+#    except:
+#        return np.nan
 
-    Markers used: `LHip`, `RHip`, `Thorax_axis` or `Pelvis_axis`
+
+def findL5_Pelvis(frame):
+    """Calculate L5 Markers Given Pelvis function
+
+    Markers used: `LHip`, `RHip`, `Pelvis_axis`
 
     Parameters
     ----------
     frame : dict
-        axis: a (4x4) array of the (x, y, z) coordinates of the axis
-        LHip: position of the left hip
-        RHip: position of the right hip
+        Dictionaries of marker lists.
 
     Returns
     -------
@@ -93,221 +320,86 @@ def find_L5(frame):
     Examples
     --------
     >>> import numpy as np
-    >>> from .kinetics import find_L5
-    >>> Pelvis_axis = [[251.74, 392.72, 1032.78, 0],
-    ...               [250.61, 391.87, 1032.87, 0],
-    ...               [251.60, 391.84, 1033.88, 0],
-    ...               [0, 0, 0, 1]]
+    >>> from .pycgmKinetics import findL5_Pelvis
+    >>> Pelvis_axis = [np.array([251.60, 391.74, 1032.89]),
+    ...                np.array([[251.74, 392.72, 1032.78],
+    ...                    [250.61, 391.87, 1032.87],
+    ...                    [251.60, 391.84, 1033.88]]),
+    ...                np.array([231.57, 210.25, 1052.24])]
     >>> LHip = np.array([308.38, 322.80, 937.98])
     >>> RHip = np.array([182.57, 339.43, 935.52])
-    >>> frame = {'axis': Pelvis_axis, 'RHip': RHip, 'LHip': LHip}
-    >>> np.around(find_L5(frame), 2) #doctest: +NORMALIZE_WHITESPACE
+    >>> frame = { 'Pelvis_axis': Pelvis_axis, 'RHip': RHip, 'LHip': LHip}
+    >>> np.around(findL5_Pelvis(frame), 2) #doctest: +NORMALIZE_WHITESPACE
     array([[ 245.48,  331.12,  936.75],
            [ 271.53,  371.69, 1043.8 ]])
     """
-    z_axis = frame['axis'][2][0:3]
-
-    norm_dir = np.array(np.multiply(z_axis, 1/np.sqrt(np.dot(z_axis, z_axis))))
-
+    # The L5 position is estimated as (LHJC + RHJC)/2 +
+    # (0.0, 0.0, 0.828) * Length(LHJC - RHJC), where the value 0.828
+    # is a ratio of the distance from the hip joint centre level to the
+    # top of the lumbar 5: this is calculated as in teh vertical (z) axis
     LHJC = frame['LHip']
     RHJC = frame['RHip']
-
     midHip = (LHJC+RHJC)/2
-    mid = np.subtract(LHJC, RHJC)
-    dist = np.sqrt(np.dot(mid, mid))
+    #zOffset = ([0.0,0.0,distance(RHJC, LHJC)*0.925])
+    #L5 = midHip + zOffset
 
-    offset = dist * .925
+    offset = distance(RHJC, LHJC) * .925
+    z_axis = frame['Pelvis_axis'][1][2]
+    norm_dir = np.array(unit(z_axis))
     L5 = midHip + offset * norm_dir
-    return midHip, L5
+
+    return midHip, L5  # midHip + ([0.0, 0.0, zOffset])
 
 
-# def get_kinetics(data, Bodymass):
-#    """Estimate center of mass values in the global coordinate system.
+def findL5_Thorax(frame):
+    """Calculate L5 Markers Given Thorax function.
 
-#    Estimates whole body CoM in global coordinate system using PiG scaling
-#    factors for determining individual segment CoM.
+    Markers used: `C7`, `RHip`, `LHip`, `Thorax_axis`
 
-#    Parameters
-#    -----------
-#    data : array
-#        Array of joint centres in the global coordinate system. List indices correspond
-#        to each frame of trial. Dict keys correspond to name of each joint center,
-#        dict values are arrays of (x, y, z) coordinates for each joint
-#        centre.
-#    Bodymass : float
-#        Total bodymass (kg) of subject
+    Parameters
+    ----------
+    frame : dict
+        Dictionaries of marker lists.
 
-#    Returns
-#    -------
-#    CoM_coords : 3D numpy array
-#        CoM trajectory in the global coordinate system.
+    Returns
+    -------
+    L5 : array
+        Returns the (x, y, z) marker positions of the L5 in a (1x3) array.
 
-#    Todo
-#    ----
-#    Figure out weird offset
+    Examples
+    --------
+    >>> from .pycgmKinetics import findL5_Thorax
+    >>> import numpy as np
+    >>> Thorax_axis = [[[256.34, 365.72, 1461.92], 
+    ...               [257.26, 364.69, 1462.23], 
+    ...               [256.18, 364.43, 1461.36]],
+    ...               [256.27, 364.79, 1462.29]]
+    >>> C7 = np.array([256.78, 371.28, 1459.70])
+    >>> LHip = np.array([308.38, 322.80, 937.98])
+    >>> RHip = np.array([182.57, 339.43, 935.52])
+    >>> frame = { 'C7': C7, 'RHip': RHip, 'LHip': LHip, 'Thorax_axis': Thorax_axis}
+    >>> np.around(findL5_Thorax(frame), 2) #doctest: +NORMALIZE_WHITESPACE
+    array([ 265.16,  359.12, 1049.06])
+    """
+    C7_ = frame['C7']
+    x_axis, y_axis, z_axis = frame['Thorax_axis'][0]
+    norm_dir_y = np.array(unit(y_axis))
+    if C7_[1] >= 0:
+        C7 = C7_ + 7 * -norm_dir_y
+    else:
+        C7 = C7_ + 7 * norm_dir_y
 
-#    Examples
-#    --------
-#    >>> from .helpers import getfilenames
-#    >>> from .IO import loadData, loadVSK
-#    >>> from .pycgmStatic import getStatic
-#    >>> from .calc import calcAngles
-#    >>> from numpy import around
-#    >>> dynamic_trial,static_trial,vsk_file,_,_ = getfilenames(x=3)
-#    >>> motionData  = loadData(dynamic_trial)
-#    SampleData/Sample_2/RoboWalk.c3d
-#    >>> staticData = loadData(static_trial)
-#    SampleData/Sample_2/RoboStatic.c3d
-#    >>> vsk = loadVSK(vsk_file,dict=False)
-#    >>> calSM = getStatic(staticData,vsk,flat_foot=False)
-#    >>> _,joint_centers=calcAngles(motionData,start=None,end=None,vsk=calSM,
-#    ...                            splitAnglesAxis=False,formatData=False,returnjoints=True)
-#    >>> CoM_coords = get_kinetics(joint_centers, calSM['Bodymass'])
-#    >>> around(CoM_coords[0], 2)  #doctest: +NORMALIZE_WHITESPACE
-#    array([-943.59, -3.53, 856.69])
-#    """
+    norm_dir = np.array(unit(z_axis))
+    LHJC = frame['LHip']
+    RHJC = frame['RHip']
+    midHip = (LHJC+RHJC)/2
+    offset = distance(RHJC, LHJC) * .925
 
-#    seg_scale = {}
-#    with open(os.path.dirname(os.path.abspath(__file__)) + '/segments.csv', 'r') as f:
-#        next(f)
-#        for line in f:
-#            row = line.rstrip('\n').split(',')
-#            seg_scale[row[0]] = {'com': float(row[1]), 'mass': float(
-#                row[2]), 'x': row[3], 'y': row[4], 'z': row[5]}
+    L5 = midHip + offset * norm_dir
+    return L5
 
-#    # names of segments
-#    segments = ['RFoot', 'RTibia', 'RFemur', 'LFoot', 'LTibia', 'LFemur', 'Pelvis',
-#                'RRadius', 'RHand', 'RHumerus', 'LRadius', 'LHand', 'LHumerus', 'Head', 'Thorax']
 
-#    # empty array for CoM coords
-#    CoM_coords = np.empty([len(data), 3])
-
-#    # iterate through each frame of JC
-#    # enumeration used to populate CoM_coords
-#    for ind, frame in enumerate(data):
-#        # find distal and proximal joint centres
-#        seg_temp = {}
-
-#        for seg in segments:
-#            seg_temp[seg] = {}
-
-#            # populate dict with appropriate joint centres
-#            if seg == 'LFoot' or seg == 'RFoot':
-#                seg_temp[seg]['Prox'] = frame[seg[0]+'Foot']
-#                seg_temp[seg]['Dist'] = frame[seg[0]+'HEE']
-
-#            if seg == 'LTibia' or seg == 'RTibia':
-#                seg_temp[seg]['Prox'] = frame[seg[0]+'Knee']
-#                seg_temp[seg]['Dist'] = frame[seg[0]+'Ankle']
-
-#            if seg == 'LFemur' or seg == 'RFemur':
-#                seg_temp[seg]['Prox'] = frame[seg[0]+'Hip']
-#                seg_temp[seg]['Dist'] = frame[seg[0]+'Knee']
-
-#            if seg == 'Pelvis':
-#                midHip, L5 = find_L5(frame)
-#                seg_temp[seg]['Prox'] = midHip
-#                seg_temp[seg]['Dist'] = L5
-
-#            if seg == 'Thorax':
-#                # The thorax length is taken as the distance between an
-#                # approximation to the C7 vertebra and the L5 vertebra in the
-#                # Thorax reference frame. C7 is estimated from the C7 marker,
-#                # and offset by half a marker diameter in the direction of
-#                # the X axis. L5 is estimated from the L5 provided from the
-#                # pelvis segment, but localised to the thorax.
-
-#                _, L5 = find_L5(frame)
-#                C7 = frame['C7']
-
-#                CLAV = frame['CLAV']
-#                STRN = frame['STRN']
-#                T10 = frame['T10']
-
-#                upper = np.array(np.multiply(np.add(CLAV, C7), 1/2.0))
-#                lower = np.array(np.multiply(np.add(STRN, T10), 1/2.0))
-
-#                # Get the direction of the primary axis Z (facing down)
-#                z_vec = upper - lower
-#                mag = np.sqrt(np.dot(z_vec, z_vec))
-
-#                z_dir = np.array(np.multiply(z_vec, 1/mag))
-#                new_start = upper + (z_dir * 300)
-#                new_end = lower - (z_dir * 300)
-
-#                newL5 = pnt_line(L5, new_start, new_end)
-#                newC7 = pnt_line(C7, new_start, new_end)
-
-#                seg_temp[seg]['Prox'] = np.array(newC7)
-#                seg_temp[seg]['Dist'] = np.array(newL5)
-
-#            if seg == 'LHumerus' or seg == "RHumerus":
-#                seg_temp[seg]['Prox'] = frame[seg[0]+'Shoulder']
-#                seg_temp[seg]['Dist'] = frame[seg[0]+'Humerus']
-
-#            if seg == 'RRadius' or seg == 'LRadius':
-#                seg_temp[seg]['Prox'] = frame[seg[0]+'Humerus']
-#                seg_temp[seg]['Dist'] = frame[seg[0]+'Radius']
-
-#            if seg == 'LHand' or seg == 'RHand':
-#                seg_temp[seg]['Prox'] = frame[seg[0]+'Radius']
-#                seg_temp[seg]['Dist'] = frame[seg[0]+'Hand']
-
-#            if seg == 'Head':
-#                seg_temp[seg]['Prox'] = frame['Back_Head']
-#                seg_temp[seg]['Dist'] = frame['Front_Head']
-
-#            # iterate through csv scaling values
-#            for row in list(seg_scale.keys()):
-#                scale = seg_scale[row]['com']
-#                mass = seg_scale[row]['mass']
-
-#                if seg != 'Pelvis' and seg != 'Thorax' and seg != 'Head':
-#                    seg_norm = seg[1:]
-#                else:
-#                    seg_norm = seg
-
-#                if seg_norm == row:
-#                    prox = seg_temp[seg]['Prox']
-#                    dist = seg_temp[seg]['Dist']
-
-#                    # segment length
-#                    length = prox - dist
-
-#                    # segment CoM
-#                    CoM = dist + length * scale
-#                    # CoM = prox + length * scale
-
-#                    seg_temp[seg]['CoM'] = CoM
-
-#                    # segment mass (kg)
-#                    # row[2] is mass correction factor
-#                    mass = Bodymass*mass
-#                    seg_temp[seg]['Mass'] = mass
-
-#                    # segment torque
-#                    torque = CoM * mass
-#                    seg_temp[seg]['Torque'] = torque
-
-#                    # vector
-#                    Vector = np.array(np.subtract(CoM, [0, 0, 0]))
-#                    val = Vector*mass
-#                    seg_temp[seg]['val'] = val
-
-#        vals = []
-#        if pyver == 2:
-#            forIter = seg_temp.iteritems()
-#        if pyver == 3:
-#            forIter = seg_temp.items()
-
-#        for attr, value in forIter:
-#            vals.append(value['val'])
-
-#        CoM_coords[ind, :] = sum(vals) / Bodymass
-
-#    return CoM_coords
-
-def get_kinetics(data, Bodymass):
+def getKinetics(data, Bodymass):
     """Estimate center of mass values in the global coordinate system.
 
     Estimates whole body CoM in global coordinate system using PiG scaling
@@ -343,10 +435,10 @@ def get_kinetics(data, Bodymass):
 
     Examples
     --------
-    >>> from pyCGM_Single.pyCGM_Helpers import getfilenames
-    >>> from pyCGM_Single.pycgmIO import loadData, loadVSK
-    >>> from pyCGM_Single.pycgmStatic import getStatic
-    >>> from pyCGM_Single.pycgmCalc import calcAngles
+    >>> from .pyCGM_Helpers import getfilenames
+    >>> from .pycgmIO import loadData, loadVSK
+    >>> from .pycgmStatic import getStatic
+    >>> from .pycgmCalc import calcAngles
     >>> from numpy import around
     >>> dynamic_trial,static_trial,vsk_file,_,_ = getfilenames(x=3)
     >>> motionData  = loadData(dynamic_trial)
@@ -357,7 +449,7 @@ def get_kinetics(data, Bodymass):
     >>> calSM = getStatic(staticData,vsk,flat_foot=False)
     >>> _,joint_centers=calcAngles(motionData,start=None,end=None,vsk=calSM,
     ...                            splitAnglesAxis=False,formatData=False,returnjoints=True)
-    >>> CoM_coords = get_kinetics(joint_centers, calSM['Bodymass'])
+    >>> CoM_coords = getKinetics(joint_centers, calSM['Bodymass'])
     >>> around(CoM_coords[0], 2) #doctest: +NORMALIZE_WHITESPACE
     array([-942.76, -3.58, 865.33])
     """
