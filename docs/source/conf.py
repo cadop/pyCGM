ca87c7407601f1e195c5608c977ebf144e1edc12
David Kim <33339104+dvkm@users.noreply.github.com>
2021-05-04 16:21:03 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../pycgm')) #TODO this will need to be updated when folder changes
+sys.path.insert(0, os.path.abspath('../../pyCGM_Single')) #TODO this will need to be updated when folder changes
 
 
 # -- Project information -----------------------------------------------------
