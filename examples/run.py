7ddd8c3b7d1170d45265ada71f6fd1715a2b1b86
MattGonz <mgonz1228@gmail.com>
2022-05-31 21:24:58 -0400

    add updated examples

--- examples/run.py
+++ examples/run.py
@@ -1,22 +1,19 @@
 import os
-from pycgm.model.model import Model
-from pycgm.pyCGM import PyCGM
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_dir)
-os.chdir("..") # Move current path up
+import pycgm
+
+script_dir = pycgm.get_data_dir()
 
 # Standard Model, 2 dynamic trials
-model = Model('pycgm/SampleData/Sample_2/RoboStatic.c3d', \
-             ['pycgm/SampleData/Sample_2/RoboWalk.c3d', 'pycgm/SampleData/ROM/Sample_Dynamic.c3d'], \
-              'pycgm/SampleData/Sample_2/RoboSM.vsk')
+model = pycgm.Model(os.path.join(script_dir, 'Sample_2/RoboStatic.c3d'), \
+                   [os.path.join(script_dir, 'Sample_2/RoboWalk.c3d'), os.path.join(script_dir, 'ROM/Sample_Dynamic.c3d')], \
+                    os.path.join(script_dir, 'Sample_2/RoboSM.vsk'))
 
-cgm = PyCGM(model)
+cgm = pycgm.PyCGM(model)
 cgm.run_all()
 
 # Access model output
 print(f"{model.data.dynamic.RoboWalk.axes.Pelvis.shape=}")
-print(f"{model.data.dynamic.Sample_Dynamic.angles.RHip.shape=}")
 
 # Indexing cgm
 print(f"{cgm[0].data.dynamic.RoboWalk.axes.LKnee.shape=}")
