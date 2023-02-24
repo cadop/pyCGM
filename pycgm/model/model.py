8401669133f8e123492b83e5d793c8d99df8e726
Matt Gonzalez <mgonz1228@gmail.com>
2023-02-23 20:38:23 -0500

    use static_config, allow an instance of Calcs in model init

--- pycgm/model/model.py
+++ pycgm/model/model.py
@@ -2,10 +2,10 @@ import time
 
 import numpy as np
 
-from .calc.dynamic_calculations import DynamicCalc
-from .calc.kinematics import dynamic
-from .calc.kinematics import static
-from .calc.static_calculations import StaticCalc
+from .calc.dynamic_config import DynamicConfig
+from .calc.kinematics.dynamic import CalcDynamic
+from .calc.kinematics.static import CalcStatic
+from .calc.static_config import StaticConfig
 from .dynamic_trial import DynamicTrial
 from .static_trial import StaticTrial
 from .utils.structure import structure_model
@@ -25,8 +25,16 @@ class Model():
         self.dynamic_filenames = dynamic_filenames
         self.measurement_filename = measurement_filename
 
-        self.static_calc  = StaticCalc(static_functions)
-        self.dynamic_calc = DynamicCalc(dynamic_functions)
+        if issubclass(type(static_functions), CalcStatic):
+            self.static_config = StaticConfig(static_functions.funcs)
+        else:
+            self.static_config = StaticConfig(static_functions)
+
+        if issubclass(type(dynamic_functions), CalcDynamic):
+            self.dynamic_config = DynamicConfig(dynamic_functions.funcs)
+        else:
+            self.dynamic_config = DynamicConfig(dynamic_functions)
+
         self.structure()
 
 
@@ -34,10 +42,10 @@ class Model():
         """
         Run each of the Model's trials
         """
-        self.static_trial.run(self.static_calc)
+        self.static_trial.run(self.static_config)
 
-        self.dynamic_calc.expand_parameters_from_data(self.data)
-        self.dynamic_trials.run(self.dynamic_calc)
+        self.dynamic_config.expand_parameters_from_data(self.data)
+        self.dynamic_trials.run(self.dynamic_config)
 
     def structure(self):
         start = time.time()
@@ -45,8 +53,8 @@ class Model():
         self.data = structure_model(self.static_filename,
                                     self.dynamic_filenames,
                                     self.measurement_filename,
-                                    self.static_calc, 
-                                    self.dynamic_calc)
+                                    self.static_config, 
+                                    self.dynamic_config)
         
         # TODO consider adding helpers to structure custom data
         self.data.static.calibrated.measurements.FlatFoot = 0;
@@ -56,11 +64,11 @@ class Model():
 
         # Structure static trial
         self.static_trial = StaticTrial(self.data.static)
-        self.static_calc.expand_parameters_from_data(self.data)
+        self.static_config.expand_parameters_from_data(self.data)
 
         # Load calibrated parameters
         self.dynamic_trials = DynamicTrial(self.data)
-        self.dynamic_calc.update_trial_names(self.dynamic_trials.trial_names)
+        self.dynamic_config.update_trial_names(self.dynamic_trials.trial_names)
 
 
         end = time.time()
@@ -70,43 +78,43 @@ class Model():
     def insert_static_function(self, function, index=None, before=None, after=None, replaces=None):
 
         if index is not None:
-            self.static_calc.function_set.insert(index, function)
+            self.static_config.function_set.insert(index, function)
 
         elif before is not None:
-            func_idx = self.static_calc.index_of_function(before)
-            self.static_calc.function_set.insert(func_idx, function)
+            func_idx = self.static_config.index_of_function(before)
+            self.static_config.function_set.insert(func_idx, function)
 
         elif after is not None:
-            func_idx = self.static_calc.index_of_function(after)
-            self.static_calc.function_set.insert(func_idx + 1, function)
+            func_idx = self.static_config.index_of_function(after)
+            self.static_config.function_set.insert(func_idx + 1, function)
 
         elif replaces is not None:
-            func_idx = self.static_calc.index_of_function(replaces)
-            self.static_calc.function_set[func_idx] = function
+            func_idx = self.static_config.index_of_function(replaces)
+            self.static_config.function_set[func_idx] = function
 
         else:
-            self.static_calc.function_set.append(function)
+            self.static_config.function_set.append(function)
 
         self.structure()
 
     def insert_dynamic_function(self, function, index=None, before=None, after=None, replaces=None):
 
         if index is not None:
-            self.dynamic_calc.function_set.insert(index, function)
+            self.dynamic_config.function_set.insert(index, function)
 
         elif before is not None:
-            func_idx = self.dynamic_calc.index_of_function(before)
-            self.dynamic_calc.function_set.insert(func_idx, function)
+            func_idx = self.dynamic_config.index_of_function(before)
+            self.dynamic_config.function_set.insert(func_idx, function)
 
         elif after is not None:
-            func_idx = self.dynamic_calc.index_of_function(after)
-            self.dynamic_calc.function_set.insert(func_idx + 1, function)
+            func_idx = self.dynamic_config.index_of_function(after)
+            self.dynamic_config.function_set.insert(func_idx + 1, function)
 
         elif replaces is not None:
-            func_idx = self.dynamic_calc.index_of_function(replaces)
-            self.dynamic_calc.function_set[func_idx] = function
+            func_idx = self.dynamic_config.index_of_function(replaces)
+            self.dynamic_config.function_set[func_idx] = function
 
         else:
-            self.dynamic_calc.function_set.append(function)
+            self.dynamic_config.function_set.append(function)
 
         self.structure()
